1fe6cbb6a83983f35f787c9ecec54f1ae78f13a2
Tristan Pothoven <t.pothoven@saxion.nl>
2019-04-16 16:14:02 +0200

    Reworked footer to use Material icons (and design)

--- src/views/common/NetmobielFooter.vue
+++ src/views/common/NetmobielFooter.vue
@@ -1,6 +1,33 @@
 <template>
-  <footer v-if="isVisible" class="background-primary">
-    <div class="container">
+  <footer v-if="isVisible">
+    <v-bottom-nav
+      :active.sync="selectedNav"
+      :value="true"
+      absolute
+      color="transparant"
+    >
+      <v-btn flat value="plan-trip">
+        <span>Reis plannen</span>
+        <v-icon>commute</v-icon>
+      </v-btn>
+
+      <v-btn flat value="saved">
+        <span>Bewaard</span>
+        <v-icon>favorite</v-icon>
+      </v-btn>
+
+      <v-btn flat value="community">
+        <span>Community</span>
+        <v-icon>chat</v-icon>
+      </v-btn>
+
+      <v-btn flat value="profile">
+        <span>Profiel</span>
+        <v-icon>person</v-icon>
+      </v-btn>
+    </v-bottom-nav>
+
+    <!-- <div class="container">
       <div class="row mt-2">
         <router-link to="/home" class="text-secondary text-center">
           <div class="col">
@@ -28,13 +55,18 @@
           </div>
         </router-link>
       </div>
-    </div>
+    </div> -->
   </footer>
 </template>
 
 <script>
 export default {
   name: 'Footer',
+  data: function() {
+    return {
+      selectedNav: '',
+    }
+  },
   computed: {
     isVisible: function() {
       return this.$store.getters.isFooterVisible
