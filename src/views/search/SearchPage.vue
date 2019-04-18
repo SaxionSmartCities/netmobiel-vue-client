2ff33bab51fca2f2af590d8038969f3a5d23bdee
Dylan M <dylan.matab99@gmail.com>
2019-04-18 13:30:05 +0200

    Implemented SearchPage, still need to fix the fill height with the footer.

--- src/views/search/SearchPage.vue
+++ src/views/search/SearchPage.vue
@@ -1,66 +1,45 @@
 <template>
-  <div class="container">
-    <form>
-      <div class="box-widget mt-3 mb-3 background-primary">
-        <h3 class="left-align">Plan uw reis</h3>
-        <div class="form-group row">
-          <label for="from" class="col-1 col-form-label">
-            <i class="fas fa-home"></i>
-          </label>
-          <div class="col-10">
-            <input
-              id="from"
-              type="text"
-              class="form-control"
-              placeholder="Huidige locatie"
-              required
-            />
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="from" class="col-1 col-form-label">
-            <i class="fas fa-map-marker-alt"></i>
-          </label>
-          <div class="col-10">
-            <input
-              id="from"
-              type="text"
-              class="form-control"
-              placeholder="Bestemming"
-              required
-            />
-          </div>
-        </div>
-        <div class="form-group row">
-          <label for="from" class="col-1 col-form-label">
-            <i class="fas fa-clock"></i>
-          </label>
-          <div class="col-5">
-            <input
-              id="from"
-              type="time"
-              class="form-control"
-              placeholder="Vertrektijd"
-              required
-            />
-          </div>
-          <div class="col-5">
-            <input
-              id="from"
-              type="text"
-              class="form-control"
-              placeholder="+/- 10 min"
-              required
-            />
-          </div>
-        </div>
+  <v-container fill-height class="background-primary">
+    <div class="align-center full-width box-widget background-white">
+      <v-form wrap>
+        <v-layout justify-center>
+          <v-subheader class="headline">Waar wil je heen?</v-subheader>
+        </v-layout>
+        <v-layout row>
+          <v-flex xs4 align-right>
+            <v-subheader class="font-weight-bold">Van</v-subheader>
+          </v-flex>
+          <v-flex xs8>
+            <v-text-field value="Enschede"></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout>
+          <v-flex xs4>
+            <v-subheader class="font-weight-bold">Naar</v-subheader>
+          </v-flex>
+          <v-flex xs8 class="pa-0">
+            <v-text-field value="Deventer"></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout>
+          <v-flex xs4>
+            <v-subheader class="font-weight-bold">Aankomst</v-subheader>
+          </v-flex>
+          <v-flex xs8>
+            <v-text-field value="Morgen, 10.00 uur"></v-text-field>
+          </v-flex>
+        </v-layout>
+        <v-layout ma-2 align-center justify-center>
+          <v-btn round>Plan je reis!</v-btn>
+        </v-layout>
+        <v-layout justify-center>
+          <v-icon>settings</v-icon>
 
-        <p class="text-small text-center">Meer opties</p>
-      </div>
-
-      <button class="btn btn-primary btn-block">Zoek mijn reis</button>
-    </form>
-  </div>
+          <v-subheader>Reisvoorkeuren</v-subheader>
+        </v-layout>
+      </v-form>
+    </div>
+  </v-container>
 </template>
 
 <script>
@@ -73,4 +52,6 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+
+</style>
