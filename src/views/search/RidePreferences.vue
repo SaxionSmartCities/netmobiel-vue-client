e6cbc1cc0a17c70464d89fdd400779f8ad5025b1
Wout <wout.v.m@hotmail.com>
2019-04-25 16:44:45 +0200

    Made a basic implementation of the RidePreference screen. No communication to the store yet.

--- src/views/search/RidePreferences.vue
+++ src/views/search/RidePreferences.vue
@@ -6,7 +6,9 @@
       <v-flex>
         <v-subheader class="font-weight-bold">Personen</v-subheader>
       </v-flex>
-      <v-flex></v-flex>
+      <v-flex xs9>
+        <v-text-field type="number" max="10" required value="1"></v-text-field>
+      </v-flex>
     </v-layout>
     <v-divider></v-divider>
 
@@ -14,7 +16,9 @@
       <v-flex>
         <v-subheader class="font-weight-bold">Bagage</v-subheader>
       </v-flex>
-      <v-flex></v-flex>
+      <v-flex xs9>
+        <v-select :items="bagage" attach multiple></v-select
+      ></v-flex>
     </v-layout>
     <v-divider></v-divider>
 
@@ -22,7 +26,9 @@
       <v-flex>
         <v-subheader class="font-weight-bold">Overstappen</v-subheader>
       </v-flex>
-      <v-flex></v-flex>
+      <v-flex xs9>
+        <v-select attach :items="overstappen" required></v-select>
+      </v-flex>
     </v-layout>
     <v-divider></v-divider>
 
@@ -30,14 +36,19 @@
       <v-flex>
         <v-subheader class="font-weight-bold">Sorteren</v-subheader>
       </v-flex>
-      <v-flex></v-flex>
+      <v-flex xs9>
+        <v-select attach :items="sorteren"></v-select>
+      </v-flex>
     </v-layout>
     <v-divider></v-divider>
-    <v-layout>
+
+    <v-layout justify-space-between>
       <v-flex>
         <v-subheader class="font-weight-bold">Vermijden</v-subheader>
       </v-flex>
-      <v-flex></v-flex>
+      <v-flex xs9>
+        <v-select :items="vermijden" attach multiple></v-select>
+      </v-flex>
     </v-layout>
     <v-divider></v-divider>
   </v-container>
@@ -46,7 +57,20 @@
 <script>
 export default {
   name: 'RidePreferences',
+  data: function() {
+    return {
+      //hardcoded lists, must be changed later
+      overstappen: ['Ja', 'Nee'],
+      sorteren: ['Reistijd', 'Prijs', 'Bekenden'],
+      bagage: ['Buggy', 'Handbagage', 'Huisdier', 'Rollator', 'Rolstoel'],
+      vermijden: ['Bus', 'Trein', 'Lopen', 'Fiets'],
+    }
+  },
 }
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.v-text-field > .v-input__control > .v-input__slot:before {
+  border-style: none;
+}
+</style>
