d30da1ea8955e65af23fef6fc276b3b6f93873ff
Tristan Pothoven <t.pothoven@saxion.nl>
2019-05-07 16:16:45 +0200

    Added call to profile service api.

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,13 +1,22 @@
 import axios from 'axios'
 
-var BASE_URL = 'http://localhost:8080'
+var BASE_URL = 'https://dev.netmobiel.eu/gwapi'
 
 export default {
   submitRegistrationRequest: (context, payload) => {
     context.commit('storeRegistrationRequest', payload)
 
-    axios
-      .post(BASE_URL + '/someApi/someCall', context.state.registrationRequest)
+    var axiosConfig = {
+      method: 'POST',
+      url: BASE_URL + '/profiles',
+      data: context.state.registrationRequest,
+      headers: {
+        'X-Gravitee-Api-Key': '3a4516db-ece3-4477-876f-6c1a9d4d723c',
+      },
+    }
+
+    console.log(axiosConfig)
+    axios(axiosConfig)
       .then(function(response) {
         console.log(response)
       })
