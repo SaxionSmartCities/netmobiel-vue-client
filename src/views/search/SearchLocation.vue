8b7ccf6e99160e89eb6f9be12d655ae6d859b550
Dylan M <dylan.matab99@gmail.com>
2019-05-23 14:26:37 +0200

    - Implemented the text highlighting in bold when typing in the searchInput. - Also added a border radius scss variable for the searchInput.

--- src/views/search/SearchLocation.vue
+++ src/views/search/SearchLocation.vue
@@ -1,17 +1,32 @@
 <template>
-  <v-container>
+  <v-container fluid>
     <v-layout>
       <v-text-field
+        v-model="searchInput"
         outline
         prepend-inner-icon="search"
         clearable
         placeholder="Zoek..."
+        class="border-radius-input"
         single-line
       ></v-text-field>
     </v-layout>
     <v-layout>
-      <v-list>
-      </v-list>
+      <v-flex xs12>
+        <v-list pt-0>
+          <template v-for="(location, index) in filteredList">
+            <v-list-tile :key="location.name">
+              <v-list-tile-content class="grey--text">
+                <v-list-tile-title>
+                  <div v-html="highlight(location.name)"></div>
+                </v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+            <v-divider v-if="index + 1 < filteredList.length" :key="index">
+            </v-divider>
+          </template>
+        </v-list>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -19,12 +34,33 @@
 <script>
 export default {
   name: 'SearchLocation',
+  data() {
+    return {
+      searchInput: '',
+    }
+  },
   computed: {
     locations() {
       return this.$store.getters.getLocations
+    },
+    filteredList() {
+      return this.locations.filter(item => {
+        return item.name.includes(this.searchInput)
+      })
+    },
+  },
+  methods: {
+    highlight(str) {
+      return str.replace(new RegExp(this.searchInput, 'gi'), match => {
+        return '<span class="font-weight-bold">' + match + '</span>'
+      })
     },
   },
 }
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.theme--light.v-text-field--outline > .v-input__control > .v-input__slot {
+  border-radius: $form-border-radius-input;
+}
+</style>
