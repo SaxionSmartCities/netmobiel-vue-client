93097df28fd7923d561bcc939d5e1c3e9638bd35
Wout <wout.v.m@hotmail.com>
2019-06-06 16:32:17 +0200

    First implementation of 'doelen' screen. No routing to this component yet

---
+++ src/views/marketplace/GoalIteam.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-layout column travel-card ma-2>
+    <v-flex>
+      <v-layout row align-center>
+        <v-flex xs4 ma-4>
+          <v-img aspect-ratio="1" :src="itemObj.image"></v-img>
+        </v-flex>
+        <v-flex>
+          <v-layout column align-end>
+            <v-flex pr-4>
+              <h3>{{ itemObj.name }}</h3>
+              <p>{{ itemObj.address }}</p>
+            </v-flex>
+          </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-flex>
+    <v-flex>
+      <v-layout>
+        <v-flex mx-5>
+          <v-progress-linear
+            :value="calculateProgress()"
+            color="green"
+          ></v-progress-linear>
+        </v-flex>
+      </v-layout>
+      <v-layout align-center column>
+        <v-flex>
+          <v-subheader
+            >Nog
+            {{ itemObj.goalCredits - itemObj.currentCredits }}
+            credits</v-subheader
+          >
+        </v-flex>
+        <v-flex mb-2>
+          <v-btn round>Doel steunen</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'GoalIteam',
+  props: ['item'],
+  data: function() {
+    return {
+      itemObj: this.item,
+    }
+  },
+  methods: {
+    calculateProgress: function() {
+      return (this.itemObj.currentCredits / this.itemObj.goalCredits) * 100
+    },
+  },
+}
+</script>
+
+<style scoped></style>
---
+++ src/views/marketplace/GoalsHomepage.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-container>
+    <v-layout column>
+      <v-flex>
+        <p class="headline">{{ title }}</p>
+      </v-flex>
+      <v-divider></v-divider>
+      <v-flex mt-4
+        ><v-text-field
+          v-model="searchInput"
+          outline
+          prepend-inner-icon="search"
+          clearable
+          placeholder="Zoeken"
+          class="border-radius-input"
+          single-line
+        ></v-text-field
+      ></v-flex>
+      <v-flex mb-3>
+        <h1 class="font-weight-bold subheading mt-2">Populair in de buurt</h1>
+      </v-flex>
+      <v-flex>
+        <v-layout row class="scrolling-wrapper-flexbox">
+          <goal-iteam
+            v-for="i in items"
+            :key="i.name"
+            :item="i"
+            class="item"
+          ></goal-iteam>
+        </v-layout>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import GoalIteam from './GoalIteam'
+export default {
+  name: 'GoalsHomepage',
+  components: { GoalIteam },
+  data: function() {
+    return {
+      searchInput: '',
+      title: 'Doelen',
+      items: [
+        {
+          name: 'Renovatie Clubhuis',
+          address: 'ZTV, Zieuwent',
+          image:
+            'https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500',
+          currentCredits: '100',
+          goalCredits: '200',
+        },
+        {
+          name: 'Macbook Wout',
+          address: 'Enschede',
+          image:
+            'https://images.pexels.com/photos/2148217/pexels-photo-2148217.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=2&h=650&w=940',
+          currentCredits: '8',
+          goalCredits: '4000',
+        },
+        {
+          name: 'Renovatie Clubhuis2',
+          address: 'ZTV, Zieuwent',
+          image:
+            'https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500',
+          currentCredits: '100',
+          goalCredits: '200',
+        },
+        {
+          name: 'Renovatie Clubhuis3',
+          address: 'ZTV, Zieuwent',
+          image:
+            'https://images.pexels.com/photos/2102587/pexels-photo-2102587.jpeg?auto=format%2Ccompress&cs=tinysrgb&dpr=1&w=500',
+          currentCredits: '100',
+          goalCredits: '200',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+.scrolling-wrapper-flexbox {
+  overflow-x: auto;
+  .item {
+    flex: 0 0 auto;
+  }
+}
+</style>
