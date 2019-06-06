fc739e2f852a7f361111c69a86df68914307653a
Lieke Nijland <lieke.nijland98@gmail.com>
2019-06-06 13:51:16 +0200

    fixed scaling

--- src/views/profile/ProfilePage.vue
+++ src/views/profile/ProfilePage.vue
@@ -1,30 +1,43 @@
 <template>
   <v-container>
     <v-layout column>
-      <v-layout row mb-4>
-        <v-flex lg1 sm2>
-          <v-avatar id="profileImage" :size="size">
-            <img :src="avatar" />
-          </v-avatar>
-        </v-flex>
-        <v-layout class="mt-2">
+      <v-flex>
+        <v-layout row my-3 mr-3>
+          <v-flex lg1 sm2 xs4>
+            <v-avatar id="profileImage" :size="size">
+              <img :src="avatar" />
+            </v-avatar>
+          </v-flex>
+
           <v-flex>
-            <p class="ma-0">{{ getUserData().fullName }}</p>
-            <p class="ma-0">{{ address }}</p>
-            <v-layout row>
+            <v-layout class="mt-2">
               <v-flex>
-                <v-rating
-                  v-model="rating"
-                  background-color="yellow darken-3"
-                  color="yellow darken-3"
-                  :length="getStars"
-                  small
-                ></v-rating>
+                <p class="ma-0">{{ getUserData().fullName }}</p>
+                <p class="ma-0">{{ address }}</p>
+                <v-layout row>
+                  <v-flex>
+                    <v-rating
+                      v-model="rating"
+                      background-color="yellow darken-3"
+                      color="yellow darken-3"
+                      :length="getStars"
+                      small
+                    ></v-rating>
+                  </v-flex>
+                </v-layout>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+
+          <v-flex lg1>
+            <v-layout align-center justify-end fill-height>
+              <v-flex align-center>
+                <v-icon class="align-center">chevron_right</v-icon>
               </v-flex>
             </v-layout>
           </v-flex>
         </v-layout>
-      </v-layout>
+      </v-flex>
       <v-flex>
         <v-layout row mb-2>
           <v-flex travel-card mr-2
