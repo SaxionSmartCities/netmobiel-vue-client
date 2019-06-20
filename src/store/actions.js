e53f17f197f82f53fc409e89d595e7f48b852ae3
Timothy Sealy <timothy.sealy@gmail.com>
2019-06-20 12:05:18 +0200

    Switched http to https for the geocoder in order to prevent Mixed content error (calling an http resource from an https context).

--- src/store/actions.js
+++ src/store/actions.js
@@ -92,7 +92,7 @@ export default {
   fetchGeocoderSuggestions: (context, place) => {
     const HERE_APP_ID = process.env.VUE_APP_HERE_APP_ID
     const HERE_APP_CODE = process.env.VUE_APP_HERE_APP_CODE
-    const GEOCODER_BASE_URL = `http://autocomplete.geocoder.api.here.com/6.2/suggest.json?app_id=${HERE_APP_ID}&app_code=${HERE_APP_CODE}&query=${place}&country=NLD&beginHighlight=<b>&endHighlight=</b>`
+    const GEOCODER_BASE_URL = `https://autocomplete.geocoder.api.here.com/6.2/suggest.json?app_id=${HERE_APP_ID}&app_code=${HERE_APP_CODE}&query=${place}&country=NLD&beginHighlight=<b>&endHighlight=</b>`
 
     axios
       .get(GEOCODER_BASE_URL)
@@ -108,7 +108,7 @@ export default {
   fetchGeocoderLocation: (context, locationid) => {
     const HERE_APP_ID = process.env.VUE_APP_HERE_APP_ID
     const HERE_APP_CODE = process.env.VUE_APP_HERE_APP_CODE
-    const GEOCODER_BASE_URL = `http://geocoder.api.here.com/6.2/geocode.json?app_id=${HERE_APP_ID}&app_code=${HERE_APP_CODE}&locationid=${locationid}&gen=9&jsonattributes=1`
+    const GEOCODER_BASE_URL = `https://geocoder.api.here.com/6.2/geocode.json?app_id=${HERE_APP_ID}&app_code=${HERE_APP_CODE}&locationid=${locationid}&gen=9&jsonattributes=1`
 
     axios
       .get(GEOCODER_BASE_URL)
