55d0696334a8f3fe3ddfffb5800da3d12ae14ee6
Timothy Sealy <timothy.sealy@gmail.com>
2019-06-20 10:22:41 +0200

    Scoped the geocoder suggests to places in the Netherlands.

--- src/store/actions.js
+++ src/store/actions.js
@@ -96,7 +96,7 @@ export default {
   fetchGeocoderSuggestions: (context, place) => {
     const HERE_APP_ID = process.env.VUE_APP_HERE_APP_ID
     const HERE_APP_CODE = process.env.VUE_APP_HERE_APP_CODE
-    const GEOCODER_BASE_URL = `http://autocomplete.geocoder.api.here.com/6.2/suggest.json?app_id=${HERE_APP_ID}&app_code=${HERE_APP_CODE}&query=${place}&beginHighlight=<b>&endHighlight=</b>`
+    const GEOCODER_BASE_URL = `http://autocomplete.geocoder.api.here.com/6.2/suggest.json?app_id=${HERE_APP_ID}&app_code=${HERE_APP_CODE}&query=${place}&country=NLD&beginHighlight=<b>&endHighlight=</b>`
 
     axios
       .get(GEOCODER_BASE_URL)
