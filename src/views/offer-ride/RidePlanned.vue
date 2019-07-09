41990ec5997d3c1a2116370cde3861bf1fae6e57
Wout <wout.v.m@hotmail.com>
2019-07-09 14:03:25 +0200

    first implementation of "Rit bevestigd" screen

---
+++ src/views/offer-ride/RidePlanned.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container>
+    <v-layout>
+      <v-flex>
+        <v-layout>
+          <v-flex
+            ><h2>
+              Rit gepland!
+            </h2>
+            <v-icon></v-icon>
+          </v-flex>
+          <v-flex
+            ><p>
+              Goed bezig, jouw rit wordt meegenomen in de zoekresultaten! Je
+              krijgt automatich een bericht als er iemand mee wil.
+            </p></v-flex
+          >
+        </v-layout>
+      </v-flex>
+      <v-flex>
+        <v-layout>
+          <v-flex></v-flex>
+          <v-flex></v-flex>
+        </v-layout>
+      </v-flex>
+    </v-layout> </v-container
+></template>
+
+<script>
+export default {
+  name: 'RidePlanned',
+}
+</script>
+
+<style scoped></style>
