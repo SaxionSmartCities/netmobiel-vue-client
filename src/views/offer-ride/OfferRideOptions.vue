63e2ad56a1abaf95ccaa0f0dd5f85915651e29a6
Lieke Nijland <lieke.nijland98@gmail.com>
2019-07-11 11:59:37 +0200

    cleaned code

--- src/views/offer-ride/OfferRideOptions.vue
+++ src/views/offer-ride/OfferRideOptions.vue
@@ -219,47 +219,4 @@ export default {
 #inputPersons {
   text-align: center;
 }
-
-.round label {
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 50%;
-  cursor: pointer;
-  height: 28px;
-  left: 0;
-  position: absolute;
-  top: 0;
-  width: 28px;
-}
-
-.round label:after {
-  border: 2px solid #fff;
-  border-top: none;
-  border-right: none;
-  content: '';
-  height: 6px;
-  left: 7px;
-  opacity: 0;
-  position: absolute;
-  top: 8px;
-  transform: rotate(-45deg);
-  width: 12px;
-}
-
-.round input[type='checkbox'] {
-  visibility: hidden;
-}
-
-.round input[type='checkbox']:checked + label {
-  background-color: $color-green;
-  border-color: $color-green;
-}
-
-.round input[type='checkbox']:checked + label:after {
-  opacity: 1;
-}
-
-body {
-  background-color: #f1f2f3;
-}
 </style>
