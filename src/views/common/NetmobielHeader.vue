75850f8d911c4127fdeada93e9bf5e4727a252f1
heinze havinga <h.havinga@saxion.nl>
2019-08-27 15:56:41 +0200

    Fixed header logo scaling

--- src/views/common/NetmobielHeader.vue
+++ src/views/common/NetmobielHeader.vue
@@ -26,7 +26,7 @@ export default {
 .header {
   background: $color-green;
   background-image: url('../../assets/logo_header.png');
-  background-size: 40% auto;
+  background-size: 200px;
   background-repeat: no-repeat;
   background-position: center center;
 }
