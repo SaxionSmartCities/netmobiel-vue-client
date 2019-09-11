f9d177aa42734de4aab3f2d7cd5b7fc4f4b07f62
Timothy Sealy <timothy.sealy@gmail.com>
2019-09-11 21:27:05 +0200

    Rewrite modules.export to export default.

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,6 @@
-let config = {
+export default {
   BASE_URL: 'https://dev.netmobiel.eu/gwapi',
 
   GRAVITEE_PLAN_SERVICE_API_KEY: '58328a63-b55f-4cb3-9102-1d76291e55f9',
   GRAVITEE_PROFILE_SERVICE_API_KEY: '3a4516db-ece3-4477-876f-6c1a9d4d723c',
 }
-
-module.exports = config
