826956f0a56e689285e4ab2a1ff73e052ea9fd71
Tristan Pothoven <tpothoven@users.noreply.github.com>
2019-09-13 15:53:27 +0200

    Dev (#90)
    * Added api key for geocoding service.
    * Added working geocoding actions and mutations.

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export default {
   BASE_URL: 'https://dev.netmobiel.eu/gwapi',
-
   GRAVITEE_PLAN_SERVICE_API_KEY: '58328a63-b55f-4cb3-9102-1d76291e55f9',
   GRAVITEE_PROFILE_SERVICE_API_KEY: '3a4516db-ece3-4477-876f-6c1a9d4d723c',
+  GRAVITEE_GEO_SERVICE_API_KEY: '922e683f-46a2-4d0f-83ca-68a9ca27f3d6',
 }
