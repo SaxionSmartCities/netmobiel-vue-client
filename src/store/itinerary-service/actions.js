298c37e480e517cb894ad0015c659f4cda3132dd
Timothy Sealy <timothy.sealy@gmail.com>
2019-10-03 16:02:43 +0200

    Convert date string to epochs for proper processing by travelcard (at tripoverview)

--- src/store/itinerary-service/actions.js
+++ src/store/itinerary-service/actions.js
@@ -107,6 +107,11 @@ export default {
       .get(URL, { headers: generateHeader(GRAVITEE_TRIP_SERVICE_API_KEY) })
       .then(response => {
         if (response.status == 200 && response.data.trips.length > 0) {
+          // Convert date to epochs.
+          let parsedTrips = response.data.trips.map(trip => {
+            trip.date = moment(trip.date).valueOf()
+            return trip
+          })
           context.commit('setPlannedTrips', response.data.trips)
         }
       })
