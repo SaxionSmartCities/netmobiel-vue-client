9ad4359381dcccbc9e8cb66853da0827c9288942
Tristan Pothoven <t.pothoven@saxion.nl>
2019-11-07 16:58:56 +0100

    Updated UpdateCard to be able to handle links and show buttons..

--- src/components/home/UpdateCard.vue
+++ src/components/home/UpdateCard.vue
@@ -1,13 +1,31 @@
 <template>
   <v-layout>
-    <v-flex travel-card px-3 pt-3>
+    <v-flex travel-card px-3 py-3>
       <v-layout>
         <v-flex xs1>
           <v-icon>new_releases</v-icon>
         </v-flex>
         <v-flex pl-2>
-          <h3>{{ updateMessage.title }}</h3>
-          <p>{{ updateMessage.content }}</p>
+          <v-layout column>
+            <v-flex>
+              <h3>{{ updateMessage.title }}</h3>
+            </v-flex>
+            <v-flex>
+              <p>{{ updateMessage.content }}</p>
+            </v-flex>
+            <v-flex>
+              <v-layout justify-end>
+                <v-btn
+                  v-if="updateMessage.link !== undefined"
+                  rounded
+                  outlined
+                  :to="updateMessage.link.to"
+                >
+                  {{ updateMessage.link.label }}
+                </v-btn>
+              </v-layout>
+            </v-flex>
+          </v-layout>
         </v-flex>
         <v-flex xs1 @click="popMessage">
           <v-icon>close</v-icon>
