399618060afaf6961105ef474bb30736beda7311
Timothy Sealy <timothy.sealy@gmail.com>
2019-11-08 09:05:06 +0100

    Added required api key in header for the autosuggest call.

--- src/store/geocoder-service/actions.js
+++ src/store/geocoder-service/actions.js
@@ -1,7 +1,15 @@
 import axios from 'axios'
+import config from '@/config/config'
 
 const GEOCODER_BASE_URL =
   process.env.VUE_APP_BASE_URL || 'https://dev.netmobiel.eu/gwapi'
+const GRAVITEE_GEO_SERVICE_API_KEY = config.GRAVITEE_GEO_SERVICE_API_KEY
+
+function generateHeaders(key) {
+  return {
+    'X-Gravitee-Api-Key': key,
+  }
+}
 
 export default {
   fetchGeocoderSuggestions: async (
@@ -11,6 +19,7 @@ export default {
     try {
       const resp = await axios.get(`${GEOCODER_BASE_URL}/geo/autosuggest`, {
         params: { place, in: area, result_types, hlStart, hlEnd },
+        headers: generateHeaders(GRAVITEE_GEO_SERVICE_API_KEY),
       })
       context.commit('setGeocoderSuggestions', resp.data.suggestions)
     } catch (problem) {
