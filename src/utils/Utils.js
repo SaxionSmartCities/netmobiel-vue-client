447e504041935c6a764df7557a2c02f41648153d
J-D-Heijink <j.d.heijink@saxion.nl>
2019-11-26 16:10:46 +0100

    favor ESM export over CommonJS

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,5 +1,3 @@
-module.exports = {
-  upperCaseFirst: function(input) {
-    return input.charAt(0).toUpperCase() + input.slice(1)
-  },
+export function upperCaseFirst(input) {
+  return input.charAt(0).toUpperCase() + input.slice(1)
 }
