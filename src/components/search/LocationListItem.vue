d747438ed60ef592b39389db0686872aae4eb2ce
Tristan Pothoven <t.pothoven@saxion.nl>
2019-11-28 15:39:33 +0100

    Reworked searchLocation results page.
    - SearchSuggestionInput is no longer a component but a page (SearchLocationPage has been revamped).
    - Created LocationListItem which represents 1 single line of results.
    - Improved styling of component, based on Vuetify 2.
    
    Also removed unused styling class.

---
+++ src/components/search/LocationListItem.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-list-item two-line>
+    <v-list-item-icon>
+      <v-icon>{{ iconicCategory(suggestion.category) }}</v-icon>
+    </v-list-item-icon>
+    <v-list-item-content>
+      <v-list-item-title>
+        <div class="text-truncate" v-html="suggestion.highlightedTitle"></div>
+      </v-list-item-title>
+      <v-list-item-subtitle>
+        <div
+          class="text-truncate"
+          v-html="stripBreakLines(suggestion.highlightedVicinity)"
+        ></div>
+      </v-list-item-subtitle>
+    </v-list-item-content>
+    <v-list-item-icon>
+      <v-icon>favorite</v-icon>
+    </v-list-item-icon>
+  </v-list-item>
+</template>
+
+<script>
+const categoryIcons = {
+  airport: 'local_airport',
+  'city-town-village': 'location_city',
+  hotel: 'local_hotel',
+  'railway-station': 'train',
+  restaurant: 'restaurant',
+  'sights-museums': 'museum',
+  'sports-facility-venue': 'sports',
+  'taxi-stand': 'local_taxi',
+  'theatre-music-culture': 'theaters',
+}
+
+export default {
+  name: 'LocationListItem',
+  props: {
+    suggestion: {
+      type: Object,
+      required: true,
+    },
+    hideFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+  },
+  methods: {
+    iconicCategory(category) {
+      return categoryIcons[category] || 'fa-map-marker-alt'
+    },
+    stripBreakLines(value) {
+      let stripStageOne = value.replace('<br>', '')
+      let stripStageTwo = stripStageOne.replace('<br/>', '')
+
+      return stripStageTwo
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/SearchLocationPage.vue
+++ src/pages/SearchLocationPage.vue
@@ -1,47 +1,123 @@
 <template>
-  <v-container fluid>
-    <search-suggestion-input></search-suggestion-input>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-text-field
+          v-model="searchInput"
+          autofocus
+          clearable
+          outlined
+          placeholder="Zoek..."
+          prepend-inner-icon="search"
+          outline
+          single-line
+          hide-details
+          @click:clear="clearSearchInput"
+        >
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <v-row v-if="showSuggestionsList">
+      <v-list max-width="100%">
+        <v-list-item-group>
+          <template v-for="(suggestion, index) in suggestions">
+            <location-list-item
+              :key="suggestion.id"
+              :suggestion="suggestion"
+              @click="completeSearch(suggestion)"
+            />
+            <v-divider v-if="index + 1 < suggestions.length" :key="index" />
+          </template>
+        </v-list-item-group>
+      </v-list>
+    </v-row>
+    <v-row v-else dense>
+      <v-col>
+        <v-row>
+          <v-col>
+            <span class="text-uppercase text-color-primary">
+              Mijn favorieten
+            </span>
+          </v-col>
+        </v-row>
+        <v-row> <v-col> </v-col> </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-import SearchSuggestionInput from '@/components/search/SearchSuggestionInput'
+import LocationListItem from '@/components/search/LocationListItem.vue'
+// map category to Material icon name (needs more work...)
+// show at most 8 suitable suggestions
+const highlightMarker = 'class="search-hit"'
+const skipCategories = new Set(['intersection'])
+const maxSuggestions = 8
+function improveSuggestions(suggestions) {
+  console.log(suggestions)
+  const highlighted = suggestions.filter(
+    suggestion =>
+      suggestion.highlightedTitle.indexOf(highlightMarker) > 0 &&
+      !skipCategories.has(suggestion.category)
+  )
+  highlighted.length = Math.min(highlighted.length, maxSuggestions)
+  return highlighted
+}
+
 export default {
-  name: 'SearchLocation',
-  components: { SearchSuggestionInput },
+  name: 'SearchSuggestionInput',
+  components: {
+    LocationListItem,
+  },
   data() {
     return {
       searchInput: '',
       showSuggestionsList: false,
+      favorites: [{}],
     }
   },
   computed: {
-    locations() {
-      return this.$store.getters.getGeocoderSuggestions
-    },
-    getPickedLocation() {
-      return this.$store.getters['gs/getPickedLocation']
+    suggestions() {
+      this.$store.getters['gs/getGeocoderSuggestions']
+      return improveSuggestions(
+        this.$store.getters['gs/getGeocoderSuggestions']
+      )
     },
   },
   watch: {
     searchInput: function(val) {
-      if (val) {
-        if (val.length > 3) {
-          this.showSuggestionsList = true
-          this.$store.dispatch('fetchGeocoderSuggestions', val)
+      if (val != null) {
+        const show = (this.showSuggestionsList = val.length > 3)
+        if (show) {
+          this.$store.dispatch('gs/fetchGeocoderSuggestions', {
+            place: val,
+            // geographic center of the Netherlands (near Lunteren)
+            area: '52.063045,5.349972;r=150000',
+            result_types: 'place,address',
+            // highlight the search text
+            hlStart: `<span ${highlightMarker}>`,
+            hlEnd: '</span>',
+          })
         }
       }
     },
-    getPickedLocation: function() {
-      this.$router.go(-1)
-    },
   },
-  created: function() {
+  created() {
     this.$store.commit('ui/showBackButton')
   },
   methods: {
-    completeSearch(location) {
-      this.$store.dispatch('gs/fetchGeocoderLocation', location.locationId)
+    parseHighlightedLabel(suggestion) {
+      return `${suggestion.highlightedTitle} ${suggestion.highlightedVicinity}`
+    },
+    completeSearch(suggestion) {
+      this.$store.commit('gs/setGeoLocationPicked', {
+        field: this.$route.params.field,
+        suggestion: {
+          ...suggestion,
+          vicinity: suggestion.vicinity.replace('<br/>', ' '),
+        },
+      })
+      this.$router.go(-1)
     },
     clearSearchInput() {
       this.searchInput = ''
@@ -52,7 +128,8 @@ export default {
 </script>
 
 <style lang="scss">
-.border-radius-input > .v-input__control > .v-input__slot {
-  border-radius: $border-radius;
+.search-hit {
+  font-weight: bold;
+  font-size: 110%;
 }
 </style>
