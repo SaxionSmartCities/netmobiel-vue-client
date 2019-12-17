98568d035e962decd9e1a75c719113bfaf54c5e8
J-D-Heijink <j.d.heijink@saxion.nl>
2019-12-17 15:15:29 +0100

    ride cards improvements

--- src/components/rides/RideCard.vue
+++ src/components/rides/RideCard.vue
@@ -1,7 +1,53 @@
 <template>
+  <v-container travel-card>
+    <v-row align="center">
+      <v-col cols="11">
+        <v-row align="center" class="body-2">
+          <v-col cols="7" class="pt-0 pb-0">
+            <div v-if="ride.recurrence">
+              <v-icon>replay</v-icon>
+              {{ formatRecurrence(ride) }}
+            </div>
+          </v-col>
+          <v-col cols="5" class="text-right pt-0 pb-0">
+            {{ ride.bookings.length }} passagiers
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4" class="font-weight-bold pt-0 pb-0">Vertrek</v-col>
+          <v-col cols="8" class="pt-0 pb-0 text-overflow-with-ellipsis">
+            {{ formatTime(ride.departureTime) }}
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="4" class="font-weight-bold pt-0 pb-0">Reisduur</v-col>
+          <v-col cols="8" class="pt-0 pb-0 text-overflow-with-ellipsis">
+            {{ formatDuration(ride.estimatedDrivingTime) }}
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="2" class="font-weight-bold pt-0 pb-0">Van</v-col>
+          <v-col cols="10" class="pt-0 pb-0 text-overflow-with-ellipsis">
+            {{ ride.fromPlace.label }}
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="2" class="font-weight-bold pt-0 pb-0">Naar</v-col>
+          <v-col cols="10" class="pt-0 pb-0 text-overflow-with-ellipsis">
+            {{ ride.toPlace.label }}
+          </v-col>
+        </v-row>
+      </v-col>
+      <v-col cols="1" class="pa-0" @click="showRideDetails()">
+        <v-icon>keyboard_arrow_right</v-icon>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<!--
   <v-layout>
     <v-flex travel-card pa-3>
-      <!-- REMOVED XS11 CLASS FROM FLEX ABOVE FOR ALPHA RELEAES -->
       <v-layout column>
         <v-flex>
           <v-layout>
@@ -33,17 +79,15 @@
         </v-flex>
       </v-layout>
     </v-flex>
-    <!-- DISABLED FOR ALPHA RELEASE
     <v-flex d-flex>
       <v-layout justify-center align-center column>
         <v-flex shrink>
           <v-icon @click="openDetails()">keyboard_arrow_right</v-icon>
         </v-flex>
       </v-layout>
-    </v-flex> -->
+    </v-flex>
   </v-layout>
-</template>
-
+-->
 <script>
 import moment from 'moment'
 export default {
@@ -54,23 +98,28 @@ export default {
     },
   },
   methods: {
-    formatTime: function(time) {
-      //HACK: Remove [UTC] from the date string for correct parseing.
-      // Should be fixed in the backend.
-      return moment(time.replace('[UTC]', ''))
+    formatTime(time) {
+      return moment(time)
         .locale('nl')
         .calendar()
     },
-    //HACK: Very dangerous function. Can embarass you at a cocktail party.
-    formatPlace(placeString) {
-      return placeString.split(',')[0]
+    formatDuration(seconds) {
+      const minutes = Math.round(seconds / 60)
+      return minutes < 60
+        ? `${minutes} minuten`
+        : `${Math.floor(minutes / 60)} uur ${minutes % 60} minuten`
+    },
+    formatRecurrence(ride) {
+      const { unit, interval } = ride.recurrence
+      if (unit === 'DAY') {
+        return 'Dagelijks'
+      }
+      const weekday = moment(ride.departureTime)
+          .locale('nl')
+          .format('dd'),
+        weekly = interval === 1 ? 'Wekelijks' : `Elke ${interval} weken`
+      return `${weekly} op ${weekday}`
     },
   },
 }
 </script>
-
-<style lang="scss">
-.align-right {
-  text-align: right;
-}
-</style>
