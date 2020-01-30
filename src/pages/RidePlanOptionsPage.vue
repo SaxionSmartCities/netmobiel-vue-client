f1a514b740cdae39717c999f3557b0d3d8b2434f
Timothy Sealy <timothy.sealy@gmail.com>
2020-01-30 14:22:16 +0100

    Refactored page to use the search icon expansion panel component. Also cleaned up the code a bit.

--- src/pages/RidePlanOptionsPage.vue
+++ src/pages/RidePlanOptionsPage.vue
@@ -1,13 +1,15 @@
 <template>
   <content-pane>
-    <template v-slot:header>
-      <h1 class="pl-3 pt-3">
-        Riteigenschappen
-      </h1>
-    </template>
-    <v-row no-gutters>
+    <v-row dense>
       <v-col>
-        <v-row no-gutters>
+        <v-row dense>
+          <v-col>
+            <h1>
+              Riteigenschappen
+            </h1>
+          </v-col>
+        </v-row>
+        <v-row dense>
           <v-col>
             <v-expansion-panels accordion>
               <v-expansion-panel>
@@ -21,7 +23,7 @@
                       <span class="form-label">Max. aantal passagiers</span>
                     </v-col>
                     <v-col cols="1" class="text-right">
-                      {{ numPassengers }}
+                      {{ ridePlanOptions.numPassengers }}
                     </v-col>
                   </v-row>
                 </v-expansion-panel-header>
@@ -30,7 +32,7 @@
                     <v-col>
                       <v-slider
                         v-if="maxNrOfPersons > 1"
-                        v-model="numPassengers"
+                        v-model="ridePlanOptions.numPassengers"
                         class="px-4"
                         thumb-color="thumb-grey"
                         thumb-label
@@ -44,39 +46,14 @@
                   </v-row>
                 </v-expansion-panel-content>
               </v-expansion-panel>
-              <v-expansion-panel>
-                <v-expansion-panel-header>
-                  <v-row no-gutters justify="space-between">
-                    <v-col align-self="center">
-                      <span class="form-label">Bagage</span>
-                    </v-col>
-                    <v-col cols="7" class="text-right">
-                      <span
-                        v-for="luggage in luggageSelected"
-                        :key="luggage.type"
-                      >
-                        <v-icon> {{ luggage.icon }}</v-icon>
-                      </span>
-                    </v-col>
-                  </v-row>
-                </v-expansion-panel-header>
-                <v-expansion-panel-content>
-                  <v-row dense>
-                    <v-col
-                      v-for="luggage in luggageTypes"
-                      :key="luggage.type"
-                      :cols="6"
-                    >
-                      <v-checkbox
-                        v-model="luggageSelected"
-                        hide-details
-                        :label="luggage.label"
-                        :value="luggage"
-                      />
-                    </v-col>
-                  </v-row>
-                </v-expansion-panel-content>
-              </v-expansion-panel>
+              <search-options-icon-expansion-panel
+                v-model="luggage"
+                @onChanged="
+                  newLugagge => {
+                    luggage = newLugagge
+                  }
+                "
+              />
               <v-expansion-panel>
                 <v-expansion-panel-header>
                   <v-row no-gutters>
@@ -84,7 +61,7 @@
                       <span class="form-label py-2">Maximale omrijtijd</span>
                     </v-col>
                     <v-col cols="3" class="text-right">
-                      {{ maxMinutesDetour }} min
+                      {{ ridePlanOptions.maxMinutesDetour }} min
                     </v-col>
                   </v-row>
                 </v-expansion-panel-header>
@@ -92,13 +69,13 @@
                   <v-row dense>
                     <v-col>
                       <v-slider
-                        v-model="maxMinutesDetour"
+                        v-model="ridePlanOptions.maxMinutesDetour"
                         class="px-4"
                         thumb-color="thumb-grey"
                         thumb-label
                         ticks="always"
                         tick-size="2"
-                        :tick-labels="generateMinuteRange"
+                        :tick-labels="detourOptions"
                         min="0"
                         max="20"
                         step="5"
@@ -124,95 +101,66 @@
 
 <script>
 import ContentPane from '@/components/common/ContentPane.vue'
+import SearchOptionsIconExpansionPanel from '@/components/search/SearchOptionsIconExpansionPanel.vue'
 import luggageTypes from '@/constants/luggage-types.js'
 
 export default {
   name: 'RidePreferences',
   components: {
     ContentPane,
+    SearchOptionsIconExpansionPanel,
   },
-  data: function() {
+  data() {
     return {
       maxNrOfPersons: 4,
-      numPassengers: 1,
-      maxMinutesDetour: 0,
+      ridePlanOptions: {},
+      detourOptions: [0, 5, 10, 15, 20],
       luggageSelected: [],
-      image: require('@/assets/placeholder_car.png'),
-      cars: [],
-      selectedCarId: undefined,
     }
   },
   computed: {
-    luggageTypes: function() {
-      return luggageTypes
-    },
-    generatePersonRange: function() {
+    generatePersonRange() {
       let result = []
       for (let i = 1; i <= this.maxNrOfPersons; i++) {
         result.push(i)
       }
       return result
     },
-    generateMinuteRange: function() {
-      let result = []
-      for (let i = 0; i <= 20; i += 5) {
-        result.push(i)
-      }
-      return result
+    luggage: {
+      get() {
+        return {
+          title: 'Bagage',
+          options: luggageTypes,
+          selected: this.ridePlanOptions.luggageOptions.map(
+            o => luggageTypes[o]
+          ),
+        }
+      },
+      set(selection) {
+        this.ridePlanOptions.luggageOptions = selection.selected.map(
+          option => option.type
+        )
+      },
     },
   },
-  created: function() {
+  created() {
     this.$store.commit('ui/showBackButton')
-  },
-  mounted() {
-    let profile = this.$store.getters['ps/getProfile']
-    this.numPassengers = profile.ridePlanOptions.numPassengers
-    this.luggageSelected = profile.ridePlanOptions.luggageOptions.map(
-      option => luggageTypes[option]
-    )
-    this.maxMinutesDetour = profile.ridePlanOptions.maxMinutesDetour
-    this.cars = profile.ridePlanOptions.cars
-    this.selectedCarId = profile.ridePlanOptions.selectedCarId
-    let car = this.cars.find(car => car.id === this.selectedCarId)
-    if (car) {
-      this.maxNrOfPersons = car.nrSeats - 1
-    } else {
-      // Some default.
-      this.maxNrOfPersons = 4
+    // Clone search preferences so we can pass it as v-model and persist it
+    // when onSearchOptionsSave is emitted.
+    this.ridePlanOptions = {
+      ...this.$store.getters['ps/getProfile'].ridePlanOptions,
     }
-    if (this.numPassengers > this.maxNrOfPersons) {
-      this.numPassengers = this.maxNrOfPersons
+    if (this.ridePlanOptions.numPassengers > this.maxNrOfPersons) {
+      this.ridePlanOptions.numPassengers = this.maxNrOfPersons
     }
   },
   methods: {
-    save: function() {
-      let payload = {
-        luggageOptions: this.luggageSelected.map(x => x.type),
-        numPassengers: this.numPassengers,
-        maxMinutesDetour: this.maxMinutesDetour,
-        selectedCarId: this.selectedCarId,
-        cars: this.cars,
-      }
-      this.$store.commit('ps/setRidePlanOptions', payload)
-      let profile = this.$store.getters['ps/getProfile']
-      this.$store.dispatch('ps/updateProfile', profile)
+    save() {
+      this.$store.dispatch('ps/storeRidePreferences', this.ridePlanOptions)
       this.$router.go(-1)
     },
   },
 }
 </script>
 
-<style lang="scss">
-.v-expansion-panel {
-  box-shadow: none;
-}
-
-.selected {
-  background-color: $color-green;
-  color: $color-white;
-}
-
-.disable-icon-rotate * .v-expansion-panel__header__icon {
-  display: none;
-}
-</style>
+<style lang="scss"></style>
