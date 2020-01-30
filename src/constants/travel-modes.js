3e6bce18a6e556b00e9423e8d708404fd4309685
Timothy Sealy <timothy.sealy@gmail.com>
2020-01-30 15:00:12 +0100

    Merged travel mode surrogates with travel modes and simplified itnerary leg component.

--- src/constants/travel-modes.js
+++ src/constants/travel-modes.js
@@ -29,4 +29,24 @@ export default {
     label: 'Meerijden',
     icon: 'local_taxi',
   },
+  CAR: {
+    mode: 'CAR',
+    label: 'Auto',
+    icon: 'directions_car',
+  },
+  WAIT: {
+    mode: 'WAIT',
+    label: 'Wachten',
+    icon: 'timelapse',
+  },
+  FINISH: {
+    mode: 'FINISH',
+    label: 'Eindpunt',
+    icon: 'outlined_flag',
+  },
+  ARRIVAL: {
+    mode: 'ARRIVAL',
+    label: 'Aankomst',
+    icon: 'outlined_flag',
+  },
 }
