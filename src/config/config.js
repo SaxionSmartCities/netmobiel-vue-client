06cd06310fe6ba55f1059b2a8dfb9eb2963d8744
Timothy Sealy <timothy.sealy@gmail.com>
2020-02-04 11:49:29 +0100

    dev.netmobiel.eu has been deprecated. Use api.netmobiel.eu

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export default {
   BASE_URL:
-    process.env.VUE_APP_API_BASE_URL || 'https://dev.netmobiel.eu/gwapi/acc',
+    process.env.VUE_APP_API_BASE_URL || 'https://api.netmobiel.eu/gwapi/acc',
   GRAVITEE_PLANNER_SERVICE_API_KEY: process.env.VUE_APP_PLANNER_SERVICE_API_KEY,
   GRAVITEE_PROFILE_SERVICE_API_KEY: process.env.VUE_APP_PROFILE_SERVICE_API_KEY,
   GRAVITEE_GEO_SERVICE_API_KEY: process.env.VUE_APP_GEO_SERVICE_API_KEY,
