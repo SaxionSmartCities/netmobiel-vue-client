06c475e3c30e68305a763b82baf8bfabd1a51262
stan rothstegge <408335@student.saxion.nl>
2020-03-06 10:03:30 +0100

    re-added swapLocations

---
+++ src/pages/planner/TripPlanSubmitted.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-container>
+    <v-layout column justify-center fill-height>
+      <v-flex shrink mb-4>
+        <h1>Reis gepland! 🎉</h1>
+      </v-flex>
+      <v-flex shrink>
+        Jouw reis is bevestigd. We sturen je een herrinnering voor jouw vertrek.
+      </v-flex>
+      <v-flex shrink>
+        <v-btn
+          block
+          rounded
+          depressed
+          class="my-3 button"
+          @click="swapLocations()"
+        >
+          Plan direct je terugreis</v-btn
+        >
+        <v-btn block rounded depressed outlined color="primary" to="/plan">
+          Plan een nieuwe rit</v-btn
+        >
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TripPlanSubmitted',
+  data: function() {
+    return {}
+  },
+  methods: {
+    swapLocations() {
+      this.$store.commit('gs/swapLocations')
+      this.$router.push('/search')
+    },
+  },
+}
+</script>
+
+<style lang="scss"></style>
