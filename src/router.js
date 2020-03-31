71e21d6f6478b17a64b537ee9fdb2baa1c0a95fe
heinzehavinga <heinze.havinga@gmail.com>
2020-03-31 10:45:21 +0200

    Nbl 304 credit screen (#203)
    * Added router link and page file
    
    * Added credit functions to the store (profile service)
    
    * Added example data in creditHistory
    Made component for creditHistory lines
    Styled the credit page
    
    * Added transaction color, an additional transaction type, disabled buttons
    
    * Small styling adjustments
    Small lint changes
    
    * Some more small style changed
    Changed name of vue component so it starts with a capital
    
    * Some more styl changes
    
    Co-authored-by: heinze havinga <h.havinga@saxion.nl>
    Co-authored-by: Dylan M <dylan.matab99@gmail.com>

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import store from './store'
 Vue.use(Router)
 
 import CommunityOverviewPage from './pages/community/CommunityOverviewPage.vue'
+import Credits from './pages/profile/Creditspage.vue'
 import InboxPage from './pages/community/messages/InboxPage.vue'
 import ConversationPage from './pages/community/messages/ConversationPage.vue'
 import ItineraryDetailPage from './pages/planner/ItineraryDetailPage.vue'
@@ -53,6 +54,10 @@ const router = new Router({
       path: '/createUser',
       component: RegistrationPage,
       name: 'createUser',
+    },
+    {
+      path: '/credits',
+      component: Credits,
     },
     {
       path: '/profile',
