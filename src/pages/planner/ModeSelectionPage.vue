9d617fdb156fea887f70737fcc7a64903ec0e101
Dylan M <dylan.matab99@gmail.com>
2020-04-02 11:18:28 +0200

    Fixed modeSelection screen

--- src/pages/planner/ModeSelectionPage.vue
+++ src/pages/planner/ModeSelectionPage.vue
@@ -7,75 +7,80 @@
     grid-list-lg
     class="background-primary"
   >
-    <v-layout justify-center align-center>
-      <v-flex xs11 sm9 md6>
-        <v-layout column shrink>
-          <v-flex class="box-widget background-white">
-            <v-flex>
-              <v-form>
-                <v-layout>
-                  <v-flex xs12>
-                    <h1>Wat wil je doen?</h1>
-                  </v-flex>
-                </v-layout>
+    <v-row justify="center" align-content="center" class="mx-3">
+      <v-col sm="9" md="6">
+        <v-row class="flex-column shrink">
+          <v-col class="box-widget background-white py-3">
+            <v-row>
+              <v-col>
+                <v-form>
+                  <v-row>
+                    <v-col class="py-0">
+                      <h2 class="mb-2">Wat wil je doen?</h2>
+                    </v-col>
+                  </v-row>
 
-                <v-layout mt-2 column justify-center text-xs-center>
-                  <v-flex>
-                    <v-layout column>
-                      <v-flex>
-                        <v-btn
-                          large
-                          rounded
-                          depressed
-                          color="button"
-                          block
-                          to="/search"
-                        >
-                          Een reis plannen
-                        </v-btn>
-                      </v-flex>
-                      <v-flex py-0>
-                        <span class="mid-grey">
-                          Met iemand meerijden of via het OV.
-                        </span>
-                      </v-flex>
-                    </v-layout>
-                  </v-flex>
-                  <v-flex mt-4>
-                    <v-layout column>
-                      <v-flex>
-                        <v-btn
-                          large
-                          rounded
-                          depressed
-                          color="button"
-                          block
-                          to="/plan"
-                        >
-                          Een rit aanbieden
-                        </v-btn>
-                      </v-flex>
-                      <v-flex py-0>
-                        <span class="mid-grey">
-                          Zelf rijden en iemand meenemen.
-                        </span>
-                      </v-flex>
-                    </v-layout>
-                  </v-flex>
-                </v-layout>
-              </v-form>
-            </v-flex>
-          </v-flex>
-        </v-layout>
-      </v-flex>
-    </v-layout>
+                  <v-col
+                    class="mt-2 text-center flex-column py-0"
+                    justify="center"
+                  >
+                    <v-col class="pa-0">
+                      <v-row class="flex-column">
+                        <v-col class="px-0 pb-1">
+                          <v-btn
+                            large
+                            rounded
+                            depressed
+                            color="button"
+                            block
+                            to="/search"
+                          >
+                            Een reis plannen
+                          </v-btn>
+                        </v-col>
+                        <v-col class="pa-0">
+                          <span class="text-reducer-opacity body-2">
+                            Met iemand meerijden of via het OV.
+                          </span>
+                        </v-col>
+                      </v-row>
+                    </v-col>
+                    <v-col class="mt-4 pa-0">
+                      <v-row class="flex-column">
+                        <v-col class="px-0 pb-1">
+                          <v-btn
+                            large
+                            rounded
+                            depressed
+                            color="button"
+                            block
+                            to="/plan"
+                          >
+                            Een rit aanbieden
+                          </v-btn>
+                        </v-col>
+                        <v-col class="py-0">
+                          <span class="text-reducer-opacity body-2">
+                            Zelf rijden en iemand meenemen.
+                          </span>
+                        </v-col>
+                      </v-row>
+                    </v-col>
+                  </v-col>
+                </v-form>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   name: 'ModeSelectionPage',
-  mounted: function() {
+  mounted() {
     this.$store.commit('ui/setSelectedNav', 'planner')
     this.$store.commit('ui/addAppClass', 'modeSelectPage')
   },
