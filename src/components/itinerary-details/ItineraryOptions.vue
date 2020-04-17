b7c17ccb058e475ab02d0b8b62899fd8e2f6dcfd
Timothy Sealy <timothy.sealy@gmail.com>
2020-04-17 14:56:56 +0200

    Fixed confirmation dialog and added consistency in text.

--- src/components/itinerary-details/ItineraryOptions.vue
+++ src/components/itinerary-details/ItineraryOptions.vue
@@ -20,7 +20,7 @@
         </v-col>
       </v-row>
       <v-divider></v-divider>
-      <v-row @click="deleteTrip">
+      <v-row @click="openConfirmationDialog">
         <v-col cols="2">
           <v-icon>fa-times-circle</v-icon>
         </v-col>
@@ -31,11 +31,11 @@
       <v-dialog v-model="dialog" persistent>
         <template v-slot:activator="{ on }"> </template>
         <v-card>
-          <v-card-title class="headline">Annuleer deze rit.</v-card-title>
+          <v-card-title class="headline">Annuleer deze reis</v-card-title>
           <v-card-text>
             <v-row>
               <v-col>
-                Weet je zeker dat je deze rit wilt annuleren? Dit kan niet
+                Weet je zeker dat je deze reis wilt annuleren? Dit kan niet
                 ongedaan gemaakt worden.
               </v-col>
             </v-row>
@@ -48,9 +48,9 @@
                   mb-4
                   depressed
                   color="button"
-                  @click="closeConfirmation"
+                  @click="confirmTripCancellation"
                 >
-                  Rit annuleren
+                  Reis annuleren
                 </v-btn>
               </v-col>
             </v-row>
@@ -64,9 +64,9 @@
                   mb-4
                   depressed
                   color="primary"
-                  @click="closeDialog"
+                  @click="closeConfirmationDialog"
                 >
-                  Rit toch bewaren
+                  Reis toch bewaren
                 </v-btn>
               </v-col>
             </v-row>
@@ -91,22 +91,25 @@ export default {
     },
   },
   methods: {
-    deleteTrip() {
+    editTrip() {
+      // eslint-disable-next-line
+      console.log('Method not implemented!')
+    },
+    replanSameRoute() {
+      // eslint-disable-next-line
+      console.log('Method not implemented!')
+    },
+    openConfirmationDialog() {
+      this.dialog = true
+    },
+    confirmTripCancellation() {
+      this.closeConfirmationDialog()
       this.$store.dispatch('is/deleteSelectedTrip', {
         tripId: this.selectedTrip.id,
       })
       this.$router.push('/tripCancelledPage')
     },
-    editTrip() {},
-    replanSameRoute() {},
-    openConfirmation() {
-      this.dialog = true
-    },
-    closeConfirmation() {
-      this.dialog = false
-      this.$router.push('/tripCancelledPage')
-    },
-    closeDialog() {
+    closeConfirmationDialog() {
       this.dialog = false
     },
   },
