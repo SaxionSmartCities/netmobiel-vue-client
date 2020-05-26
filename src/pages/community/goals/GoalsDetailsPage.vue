56d2131b9ec190849ef0d0adcc7a87ab567ebd2b
Timothy Sealy <timothy.sealy@gmail.com>
2020-05-26 17:03:19 +0200

    Revamped goal details screen.

--- src/pages/community/goals/GoalsDetailsPage.vue
+++ src/pages/community/goals/GoalsDetailsPage.vue
@@ -1,36 +1,38 @@
 <template>
   <content-pane>
     <v-row align="center" justify="center">
-      <v-img
-        src="@/assets/achterhoek_background.jpg"
-        lazy-src="https://picsum.photos/id/11/100/60"
-        aspect-ratio="1"
-        class="grey lighten-2"
-        max-width="500"
-        max-height="300"
-      >
-        <template v-slot:placeholder>
-          <v-row class="fill-height ma-0" align="center" justify="center">
-            <v-progress-circular
-              indeterminate
-              color="grey lighten-5"
-            ></v-progress-circular>
-          </v-row>
-        </template>
-      </v-img>
+      <v-col>
+        <v-img
+          src="@/assets/achterhoek_background.jpg"
+          lazy-src="https://picsum.photos/id/11/100/60"
+          aspect-ratio="1"
+          class="goal-jumbo-image"
+          max-width="500"
+          max-height="300"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
+      </v-col>
     </v-row>
     <v-row>
-      <h1 class="netmobiel">
-        {{ selectedGoal.title }}
-      </h1>
-    </v-row>
-    <v-row class="mb-4">
-      <span class="overline">{{ selectedGoal.location }}</span>
+      <v-col pt-0>
+        <h1 class="netmobiel">
+          {{ selectedGoal.title }}
+        </h1>
+        <span class="overline">{{ selectedGoal.location }}</span>
+      </v-col>
     </v-row>
     <v-row>
-      <span>
-        {{ selectedGoal.description }}
-      </span>
+      <v-col>
+        <span>{{ selectedGoal.description }}</span>
+      </v-col>
     </v-row>
     <v-row>
       <v-col>
@@ -58,23 +60,21 @@
       </v-col>
     </v-row>
     <v-row>
-      <v-btn large rounded block mb-4 depressed color="button">
-        Steun dit doel
-      </v-btn>
+      <v-col>
+        <v-btn large rounded block mb-4 depressed color="button">
+          Steun dit doel
+        </v-btn>
+      </v-col>
     </v-row>
-    <v-container>
-      <v-row mb-3 mt-3>
-        <h4 class="netmobiel">
+    <v-row>
+      <v-col>
+        <h4 class="netmobiel my-3">
           Top donateurs
         </h4>
-      </v-row>
-      <v-row>
         <v-divider />
-      </v-row>
-      <v-row>
-        <donors-list :donors="getTopDonors()"> </donors-list>
-      </v-row>
-    </v-container>
+        <donors-list :donors="donors"></donors-list>
+      </v-col>
+    </v-row>
   </content-pane>
 </template>
 
@@ -86,10 +86,7 @@ export default {
   name: 'GoalsDetailsPage',
   components: { GoalProgressBar, DonorsList, ContentPane },
   props: {
-    id: {
-      type: String,
-      required: true,
-    },
+    id: { type: String, required: true },
   },
   computed: {
     selectedGoal() {
@@ -97,22 +94,18 @@ export default {
         goal => goal.id == this.id
       )
     },
+    donors() {
+      return this.selectedGoal.donors
+    },
   },
   created() {
     this.$store.commit('ui/showBackButton')
   },
-  methods: {
-    getGoals() {
-      return this.$store.getters['gos/getGoals']
-    },
-    getTopDonors() {
-      return this.$store.getters['gos/getTopDonorsList']
-    },
-    getCreditsRemaining() {
-      this.selectedGoal.creditsGoal - this.selectedGoal.creditsDonated
-    },
-  },
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.goal-jumbo-image {
+  border-radius: 10px;
+}
+</style>
