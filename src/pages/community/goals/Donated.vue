89ca5b419ac24e16806fc42b2991b754f6d2c154
Dylan M <dylan.matab99@gmail.com>
2020-06-03 15:04:40 +0200

    Feature/doel steunen (#229)
    * WiP: still need to fix the styling of support goal
    Added supportGoal screen
    Added donated screen
    
    * Added some styling and party-icon
    
    * Added methods for two button in Donated.vue
    Small styling changes
    
    * Disable donate button when amount of credits is 0 (or negative).
    
    * Textual update.
    
    * Added emphasis on name of the donated goal.
    
    * Replaced party icon image with (textual) party icon. Also changed the order of the onclicks to satisfy the linter.
    
    * Added router path to goal overview
    
    * Pass the goal id instead of name so that we can route back to the goal details page.
    
    Co-authored-by: Timothy Sealy <timothy.sealy@gmail.com>

---
+++ src/pages/community/goals/Donated.vue
@@ -0,0 +1,89 @@
+<template>
+  <content-pane>
+    <v-row class="donated-row">
+      <v-col class="donated-col">
+        <div class="d-flex flex-row">
+          <div class="full-height align-self-center">
+            <h3>
+              Gedoneerd 🎉
+            </h3>
+          </div>
+        </div>
+        <div class="mt-6">
+          <span>
+            Jouw credits zijn gedoneerd, dankjewel! <em>{{ name }}</em> is
+            automatisch op de hoogte gebracht.
+          </span>
+        </div>
+
+        <div class="mt-8">
+          <v-btn
+            large
+            rounded
+            block
+            depressed
+            color="button"
+            class="mb-2"
+            @click="supportGoal()"
+          >
+            Steun nog een doel
+          </v-btn>
+          <v-btn
+            large
+            rounded
+            outlined
+            block
+            color="primary"
+            @click="showAllDonations()"
+          >
+            Bekijk alle donaties
+          </v-btn>
+        </div>
+      </v-col>
+    </v-row>
+  </content-pane>
+</template>
+
+<script>
+import ContentPane from '../../../components/common/ContentPane'
+export default {
+  name: 'Donated',
+  components: { ContentPane },
+  props: {
+    id: { type: String, required: true },
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    name() {
+      return (
+        this.$store.getters['gos/getGoals'].find(
+          goal => goal.id === Number(this.id)
+        ).title || 'Onbekend'
+      )
+    },
+  },
+  methods: {
+    supportGoal() {
+      this.$router.push({ name: 'goalOverviewPage' })
+    },
+    showAllDonations() {
+      this.$router.push({ path: `/goalDetails/${this.id}` })
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.donated-row {
+  height: 100%;
+  .donated-col {
+    padding-top: 10vh;
+    .party-icon {
+      margin-left: 20px;
+      max-width: 35px;
+    }
+  }
+}
+</style>
