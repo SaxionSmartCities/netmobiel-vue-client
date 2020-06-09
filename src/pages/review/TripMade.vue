7316d9f43ab07c472b0ee2cafda64982d3683bf6
Timothy Sealy <timothy.sealy@gmail.com>
2020-06-09 13:52:21 +0200

    Revamped trip made page.

--- src/pages/review/TripMade.vue
+++ src/pages/review/TripMade.vue
@@ -1,70 +1,102 @@
 <template>
-  <content-pane>
-    <v-row>
-      <v-col class="px-0 pt-0">
-        <v-chip pill>
-          <v-avatar left>
-            <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
-          </v-avatar>
-          {{ trip.legs[0].driverName }}
-        </v-chip>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="d-flex flex-column pt-0 px-0">
-        <h3>Beoordeel jouw reis</h3>
-        <span>
-          Hoe heb jij deze reis ervaren? Geef jouw mening en laat
-          {{ trip.legs[0].driverName }} weten wat jij er van vond
-        </span>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col v-if="showChips" class="px-0">
-        <v-chip
-          v-for="(compliment, index) in availableCompliments"
-          :key="index"
-          :ripple="false"
-          class="compliment-chip"
-          :class="{
-            'compliment-chip-active':
-              compliments.findIndex(c => c.value === compliment.value) !== -1,
-          }"
-          :value="compliment.value"
-          @click="addCompliment(compliment)"
-        >
-          {{ compliment.title }}
-        </v-chip>
-      </v-col>
-      <v-col v-else class="px-0">
-        <v-textarea
-          v-model="inputTextArea"
-          placeholder="Beschrijving..."
-          :hide-details="true"
-          outlined
-        ></v-textarea>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col class="px-0">
-        <v-btn class="my-2" block rounded color="button" @click="rateTrip()">
-          Beoordeel deze reis
-        </v-btn>
-        <v-btn block outlined rounded color="primary" @click="back()">
-          Terug
-        </v-btn>
-      </v-col>
-    </v-row>
-  </content-pane>
+  <v-row>
+    <v-col class="pt-0">
+      <v-row dense>
+        <v-col cols="3">
+          <round-user-image :image-size="60" :avatar-size="66">
+          </round-user-image>
+        </v-col>
+        <v-col>
+          <v-row dense>
+            <v-col>
+              <span class="subtitle-2 text-no-wrap pr-2">{{
+                trip.legs[0].driverName
+              }}</span>
+              <br />
+              <span class="overline">
+                Van {{ trip.from.label }} naar {{ trip.to.label }}
+              </span>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <h3>Beoordeel jouw reis</h3>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <span>
+            Hoe heb jij deze reis ervaren? Geef jouw mening en laat
+            {{ trip.legs[0].driverName }} weten wat jij er van vond.
+          </span>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col v-if="showChips">
+          <v-chip
+            v-for="(compliment, index) in availableCompliments"
+            :key="index"
+            :ripple="false"
+            class="compliment-chip"
+            :class="{
+              'compliment-chip-active':
+                compliments.findIndex(c => c.value === compliment.value) !== -1,
+            }"
+            :value="compliment.value"
+            @click="addCompliment(compliment)"
+          >
+            {{ compliment.title }}
+          </v-chip>
+        </v-col>
+        <v-col v-else>
+          <v-chip
+            v-for="(compliment, index) in compliments"
+            :key="index"
+            :ripple="false"
+            class="compliment-chip compliment-chip-active"
+            :value="compliment.value"
+          >
+            {{ compliment.title }}
+          </v-chip>
+          <v-textarea
+            v-model="inputTextArea"
+            class="pt-2"
+            placeholder="Beschrijving..."
+            :hide-details="true"
+            outlined
+          ></v-textarea>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn
+            class="my-2"
+            block
+            rounded
+            depressed
+            color="button"
+            @click="rateTrip()"
+          >
+            {{ showChips ? 'Volgende' : 'Beoordeel deze reis' }}
+          </v-btn>
+          <v-btn block outlined rounded color="primary" @click="back()">
+            Terug
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import ContentPane from '../../components/common/ContentPane'
+import RoundUserImage from '@/components/common/RoundUserImage'
 import trip_made_config from '../../config/review/trip_made_config'
 
 export default {
   name: 'TripMade',
-  components: { ContentPane },
+  components: { RoundUserImage },
   props: {
     trip: { type: Object, required: true },
   },
