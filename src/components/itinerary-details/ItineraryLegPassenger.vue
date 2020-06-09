3bdd14c1ae834fbfb6f94b8948522454feffdd33
Timothy Sealy <timothy.sealy@gmail.com>
2020-06-10 00:00:48 +0200

    Show passenger information if a leg has a booked passenger.

---
+++ src/components/itinerary-details/ItineraryLegPassenger.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-row no-gutters>
+    <v-col>
+      <b>{{ passenger.givenName }} {{ passenger.familyName }}</b> rijdt mee.
+      <br />
+      <round-user-image :image-size="60" :avatar-size="66"></round-user-image>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import RoundUserImage from '@/components/common/RoundUserImage'
+
+export default {
+  name: 'ItineraryLegPassenger',
+  components: { RoundUserImage },
+  props: {
+    passenger: { type: Object, required: true },
+  },
+}
+</script>
+
+<style lang="scss"></style>
