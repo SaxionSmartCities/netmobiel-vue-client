96127286bdb2bb858894b4c6607fe418ee30fa65
Timothy Sealy <timothy.sealy@gmail.com>
2020-06-23 22:09:08 +0200

    Fixed router

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import store from './store'
 Vue.use(Router)
 
 import CommunityOverviewPage from './pages/community/CommunityOverviewPage.vue'
+import Credits from './pages/profile/Creditspage.vue'
 import InboxPage from './pages/community/messages/InboxPage.vue'
 import ConversationPage from './pages/community/messages/ConversationPage.vue'
 import ItineraryDetailPage from './pages/planner/ItineraryDetailPage.vue'
@@ -17,6 +18,9 @@ import OnboardingComplete from './pages/OnboardingComplete.vue'
 import OnboardingPage from './pages/OnboardingPage.vue'
 import PrivacySecurityPage from './pages/profile/PrivacySecurityPage.vue'
 import PrivacyStatementPage from './pages/profile/PrivacyStatementPage.vue'
+import CarsPage from './pages/planner/CarsPage.vue'
+import CarAddPage from './pages/planner/CarAddPage.vue'
+import CarEditPage from './pages/planner/CarEditPage.vue'
 import ProfilePage from './pages/profile/ProfilePage.vue'
 import SearchLocationPage from './pages/planner/SearchLocationPage'
 import SearchOptionsPage from './pages/planner/SearchOptionsPage'
@@ -59,6 +63,11 @@ const router = new Router({
       path: '/createUser',
       component: RegistrationPage,
       name: 'createUser',
+    },
+    {
+      path: '/credits',
+      component: Credits,
+      name: 'credits',
     },
     {
       path: '/profile',
@@ -106,16 +115,6 @@ const router = new Router({
       path: '/planSubmitted',
       component: RidePlanSubmitted,
       name: 'planSubmitted',
-    },
-    {
-      path: '/tripPlanSubmitted',
-      component: TripPlanSubmitted,
-      name: 'tripPlanSubmitted',
-    },
-    {
-      path: '/tripReviewedPage',
-      component: TripReviewedPage,
-      name: 'tripReviewedPage',
     },
     {
       path: '/tripPlanSubmitted',
@@ -192,6 +191,21 @@ const router = new Router({
       path: '/modeSelection',
       component: ModeSelectionPage,
       name: 'modeSelectionPage',
+    },
+    {
+      path: '/Cars',
+      component: CarsPage,
+      name: 'CarsPage',
+    },
+    {
+      path: '/AddCar',
+      component: CarAddPage,
+      name: 'CarAddPage',
+    },
+    {
+      path: '/EditCar',
+      component: CarEditPage,
+      name: 'CarEditPage',
     },
     {
       path: '/onboardingPage',
