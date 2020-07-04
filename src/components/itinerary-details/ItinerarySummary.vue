f4ba7655a0453d3faebb1b548345b211f36436fa
Timothy Sealy <timothy.sealy@gmail.com>
2020-07-04 22:38:15 +0200

    Minor restyling itinerary summary.

--- src/components/itinerary-details/ItinerarySummary.vue
+++ src/components/itinerary-details/ItinerarySummary.vue
@@ -3,15 +3,23 @@
     <v-col>
       <v-row no-gutters>
         <v-col cols="3" class="bold">Datum</v-col>
-        <v-col class="departure-date">{{ printableDate }}</v-col>
+        <v-col class="departure-date pl-3">{{ printableDate }}</v-col>
       </v-row>
       <v-row no-gutters>
         <v-col cols="3" class="bold">Reisduur</v-col>
-        <v-col>{{ Math.round(duration / 60) }} minuten</v-col>
+        <v-col class="pl-3">{{ Math.round(duration / 60) }} minuten</v-col>
+      </v-row>
+      <v-row v-if="luggageTypes.length > 0" no-gutters>
+        <v-col cols="3" class="bold">Baggage</v-col>
+        <v-col class="pl-3">{{ luggageTypes }}</v-col>
       </v-row>
       <v-row v-if="cost" no-gutters>
         <v-col cols="3" class="bold">Kosten</v-col>
-        <v-col>{{ cost }} credits</v-col>
+        <v-col class="pl-3">{{ cost }} credits</v-col>
+      </v-row>
+      <v-row v-if="revenue" no-gutters>
+        <v-col cols="3" class="bold">Opbrengst</v-col>
+        <v-col class="pl-3">{{ revenue }} credits</v-col>
       </v-row>
     </v-col>
   </v-row>
@@ -26,10 +34,9 @@ export default {
   props: {
     date: { type: String, default: '' },
     duration: { type: Number, default: 0 },
+    luggageTypes: { type: Array, default: () => [] },
+    revenue: { type: Number, default: 0 },
     cost: { type: Number, default: 0 },
-  },
-  data() {
-    return {}
   },
   computed: {
     printableDate() {
