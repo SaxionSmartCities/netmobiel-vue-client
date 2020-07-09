674831dd379c5a5dfdf78bee9be0315c9de6bca1
Timothy Sealy <timothy.sealy@gmail.com>
2020-07-09 18:01:01 +0200

    Fixed submission of the rides.

--- src/pages/planner/RidePlanPage.vue
+++ src/pages/planner/RidePlanPage.vue
@@ -1,7 +1,7 @@
 <template>
   <content-pane class="background-primary">
     <v-row class="full-height justify-center align-center">
-      <v-col cols="11">
+      <v-col>
         <v-row>
           <v-col class="box-widget background-white">
             <v-form>
@@ -12,14 +12,10 @@
               </v-row>
               <v-row dense>
                 <v-col>
-                  <from-to-fields />
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col>
-                  <date-time-selector
-                    v-model="journeyMoment"
-                    :allowed-dates="allowedDates"
+                  <search-criteria
+                    v-model="searchCriteria"
+                    @locationFieldSelected="onLocationFieldSelected"
+                    @criteriaChanged="onCriteriaChanged"
                   />
                 </v-col>
               </v-row>
@@ -27,7 +23,11 @@
                 <v-col>
                   <recurrence-editor
                     v-model="recurrence"
-                    :origin="journeyMoment ? journeyMoment.when : null"
+                    :origin="
+                      searchCriteria.travelTime
+                        ? searchCriteria.travelTime.when
+                        : null
+                    "
                   />
                 </v-col>
               </v-row>
@@ -60,8 +60,9 @@
                     large
                     rounded
                     block
+                    depressed
                     color="button"
-                    :disabled="disabledRideAddition()"
+                    :disabled="disabledRideAddition"
                     @click="submitForm()"
                   >
                     Rit aanbieden
@@ -87,75 +88,118 @@
 </template>
 
 <script>
-import ContentPane from '@/components/common/ContentPane.vue'
 import moment from 'moment'
-
-import FromToFields from '@/components/common/FromToFields.vue'
-import DateTimeSelector from '@/components/common/DateTimeSelector.vue'
+import ContentPane from '@/components/common/ContentPane.vue'
+import SearchCriteria from '@/components/common/SearchCriteria.vue'
 import RecurrenceEditor from '@/components/common/RecurrenceEditor.vue'
-
 import { beforeRouteLeave, beforeRouteEnter } from '@/utils/navigation.js'
 
 export default {
   name: 'RidePlanPage',
   components: {
     ContentPane,
-    FromToFields,
-    DateTimeSelector,
+    SearchCriteria,
     RecurrenceEditor,
   },
   data() {
     return {
-      journeyMoment: undefined,
       recurrence: undefined,
     }
   },
   computed: {
+    searchCriteria() {
+      return this.$store.getters['is/getSearchCriteria']
+    },
     selectedCar() {
       const selectedCarId = this.$store.getters['ps/getProfile'].ridePlanOptions
           .selectedCarId,
         cars = this.$store.getters['cs/getAvailableCars']
       return cars.find(car => car.id === selectedCarId)
     },
+    disabledRideAddition() {
+      const { from, to, travelTime } = this.searchCriteria
+      return !from?.label || !to?.label || travelTime?.when < moment()
+    },
+    topOfTheHour() {
+      const now = moment()
+      return now.minute() || now.second() || now.millisecond()
+        ? now.add(1, 'hour').startOf('hour')
+        : now.startOf('hour')
+    },
   },
   mounted() {
     this.$store.dispatch('cs/fetchCars')
+    this.initialize()
   },
   beforeRouteEnter: beforeRouteEnter({
-    journeyMoment: DateTimeSelector.restoreModel,
     recurrence: json => json,
   }),
   beforeRouteLeave: beforeRouteLeave({
-    journeyMoment: DateTimeSelector.saveModel,
     recurrence: model => model && { ...model },
   }),
   methods: {
-    disabledRideAddition() {
+    initialize() {
       const { from, to } = this.$store.getters['gs/getPickedLocation']
-      return (
-        !from.title ||
-        !to.title ||
-        !this.journeyMoment ||
-        !this.selectedCar ||
-        this.journeyMoment.when < moment().add(1, 'hour')
-      )
+      const { travelTime } = this.searchCriteria
+      let newCriteria = {
+        ...this.searchCriteria,
+      }
+      //TODO: move mapping from geo location to geocode sevice.
+      if (from.position) {
+        newCriteria.from = {
+          label: `${from.title} ${from.vicinity || ''}`,
+          latitude: from.position[0],
+          longitude: from.position[1],
+        }
+      }
+      if (to.position) {
+        newCriteria.to = {
+          label: `${to.title} ${to.vicinity || ''}`,
+          latitude: to.position[0],
+          longitude: to.position[1],
+        }
+      }
+      if (!travelTime) {
+        // Set the default date and time to today and the next whole hour.
+        newCriteria.travelTime = {
+          when: this.topOfTheHour,
+          arriving: true,
+        }
+      }
+      this.$store.commit('is/setSearchCriteria', newCriteria)
     },
     toRidePlanOptions() {
       this.$router.push('/planOptions')
     },
+    onLocationFieldSelected(newField) {
+      this.$router.push({
+        name: 'searchLocation',
+        params: { field: newField.field, editSearchCriteria: false.toString() },
+      })
+    },
+    onCriteriaChanged(newCriteria) {
+      //TODO: Do the valid time check in the search criteria component.
+      // If the selected date is in the past show an error.
+      if (moment(newCriteria?.travelTime?.when) < moment()) {
+        this.$store.dispatch(
+          'ui/queueNotification',
+          {
+            message: 'De geselecteerde tijd ligt in het verleden.',
+            timeout: 0,
+          },
+          { root: true }
+        )
+      }
+      this.$store.commit('is/setSearchCriteria', newCriteria)
+    },
     submitForm() {
-      const { from, to } = this.$store.getters['gs/getPickedLocation']
+      const { ridePlanOptions } = this.$store.getters['ps/getProfile']
       this.$store.dispatch('cs/submitRide', {
-        from,
-        to,
-        ridePlanOptions: this.$store.getters['ps/getProfile'].ridePlanOptions,
+        ...this.searchCriteria,
         recurrence: this.recurrence,
-        timestamp: this.journeyMoment,
+        ridePlanOptions,
       })
       this.$router.push('/planSubmitted')
-    },
-    allowedDates(v) {
-      return moment(v) >= moment().startOf('day')
     },
   },
 }
