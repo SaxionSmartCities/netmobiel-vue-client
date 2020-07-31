1fb3e2e5d712d1636d32396c04e24afd252a4d32
Dylan <dylan.matab99@gmail.com>
2020-07-31 12:03:23 +0200

    Cleaned some code Added some code comments

--- src/store/index.ts
+++ src/store/index.ts
@@ -9,13 +9,16 @@ import notificationService from './notification-service'
 import profileService from './profile-service'
 import registrationService from './registration-service'
 import goalsService from './goals-service'
+import { RootState, storeBuilder } from './Rootstate'
 import './ui'
 
+/**
+ * Now using both the normal vuex store than can be accessed through this.$store...
+ * The vuexStore can be accessed by import the store it's index as following
+ * 'import * as uiStore from '@/store/ui''
+ */
 Vue.use(Vuex)
-
-import { RootState, storeBuilder } from './Rootstate'
-
-export const stbuilder = storeBuilder.vuexStore()
+storeBuilder.vuexStore()
 
 export default new Vuex.Store<RootState>({
   strict: process.env.NODE_ENV !== 'production',
--- src/store/ui/index.ts
+++ src/store/ui/index.ts
@@ -1,6 +1,5 @@
 import { UiState } from './types'
-import { RootState, storeBuilder } from '@/store/Rootstate'
-import { BareActionContext } from 'vuex-typex'
+import { storeBuilder } from '@/store/Rootstate'
 import uiGetters from '@/store/ui/getters'
 import uiMutations from '@/store/ui/mutations'
 import uiActions from '@/store/ui/actions'
@@ -9,24 +8,3 @@ export const uiBuilder = storeBuilder.module<UiState>('ui', new UiState())
 export const getters = uiGetters
 export const mutations = uiMutations
 export const actions = uiActions
-
-// export const getters: {
-//
-//   'isHeaderVisible': state => state.header.visible,
-//
-//   isFooterVisible: state => state.footer.visible,
-//   isBackButtonVisible: state => state.backButtonVisible,
-//   getNotificationQueue: state => state.notificationQueue,
-//   isNotificationBarVisible: state => state.notificationBarVisible,
-//   getSelectedNav: state => state.footer.selectedNav,
-//   getUpdateMessages: state => state.updateMessages,
-//
-// }
-
-// export default {
-//   namespaced,
-//   state,
-//   actions,
-//   getters,
-//   mutations,
-// }
