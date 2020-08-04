3101978fcaf84ff5b21b8411ca19335c77b5482a
Dylan M <dylan.matab99@gmail.com>
2020-08-04 14:12:33 +0200

    Charities can now be fetched

--- src/store/charity-service/actions.ts
+++ src/store/charity-service/actions.ts
@@ -1,14 +1,19 @@
 import { CharityState } from './types'
 import { RootState } from '@/store/Rootstate'
 import { BareActionContext, getStoreBuilder, ModuleBuilder } from 'vuex-typex'
+import axios from 'axios'
+import { mutations } from '@/store/charity-service/index'
 
 // @ts-ignore
 const chsBuilder: ModuleBuilder = getStoreBuilder().module('chs')
-
 type ActionContext = BareActionContext<CharityState, RootState>
 
+const BASE_URL = 'http://localhost:3000'
+
 function fetchCharities(context: ActionContext, params: any): void {
-  console.log('fetching charities')
+  axios.get(BASE_URL + '/api/charity').then(resp => {
+    mutations.setCharities(resp.data.charities)
+  })
 }
 
 const actions = {
