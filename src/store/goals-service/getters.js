9dffe0441bc6d320535a418e875b1f39201e0c56
Dylan M <dylan.matab99@gmail.com>
2020-08-10 12:17:05 +0200

    removed goals-service because it was no longer needed. The charity-service has been implemented

--- src/store/goals-service/getters.js
+++
@@ -1,5 +0,0 @@
-export default {
-  getGoals: state => state.storedGoals,
-  getTopDonorsList: state => state.topDonors,
-  getSelectedGoalIndex: state => state.storedGoals[state.selectedGoalIndex],
-}
