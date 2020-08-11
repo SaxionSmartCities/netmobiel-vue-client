5cd0492c8fb85efb9e163afd95a61efeb5004125
Timothy Sealy <timothy.sealy@gmail.com>
2020-08-11 12:53:13 +0200

    Rearranged order for my own peace of mind ;)

--- src/store/Rootstate.ts
+++ src/store/Rootstate.ts
@@ -3,15 +3,15 @@ import { UiState } from './ui/types'
 import { CharityState } from '@/store/charity-service/types'
 import { CarpoolState } from '@/store/carpool-service/types'
 import { ProfileState } from '@/store/profile-service/types'
-import { NotificationState } from '@/store/notification-service/types'
 import { GeoCoderState } from '@/store/geocoder-service/types'
 import { ItineraryState } from '@/store/itinerary-service/types'
 import { MessageState } from '@/store/message-service/types'
+import { NotificationState } from '@/store/notification-service/types'
 import { RegistrationState } from '@/store/registration-service/types'
 
 export interface RootState {
-  cs: CarpoolState
   chs: CharityState
+  cs: CarpoolState
   gs: GeoCoderState
   is: ItineraryState
   ms: MessageState
