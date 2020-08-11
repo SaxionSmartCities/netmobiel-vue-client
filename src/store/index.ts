cf669d46efa10e4a664cae8e1c5745d8994318e0
Timothy Sealy <timothy.sealy@gmail.com>
2020-08-11 12:52:40 +0200

    Removed duplicate ui import and added export of the correct store.

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,8 @@
 import Vue from 'vue'
-import Vuex from 'vuex'
+import Vuex, { Store } from 'vuex'
 
 import { RootState, storeBuilder } from './Rootstate'
 
-// NEED TO IMPORT THE STRONGLY TYPES STORES HERE, ELSE THE STORES WON'T BE BUILT!!!
-import './ui'
 import './ui'
 import './charity-service'
 import './carpool-service'
@@ -18,13 +16,10 @@ import './itinerary-service'
 /**
  * Now using both the normal vuex store than can be accessed through this.$store...
  * The vuexStore can be accessed by import the store it's index as following
- * 'import * as uiStore from '@/store/ui''
+ * `import * as uiStore from '@/store/ui'`
  */
 Vue.use(Vuex)
-
-export default storeBuilder.vuexStore()
-
-// new Vuex.Store<RootState>({
-//   strict: process.env.NODE_ENV !== 'production',
-//   modules: {},
-// })
+const store: Store<RootState> = storeBuilder.vuexStore()
+// @ts-ignore
+store.strict = process.env.NODE_ENV !== 'production'
+export default store
