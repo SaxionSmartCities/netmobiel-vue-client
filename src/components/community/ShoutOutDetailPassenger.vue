e62ebe8e162512103584039e95de1562b80e754a
Timothy Sealy <timothy.sealy@gmail.com>
2020-08-19 15:43:02 +0200

    Select offer to show trip (cancel and confirm buttons are then shown).

--- src/components/community/ShoutOutDetailPassenger.vue
+++ src/components/community/ShoutOutDetailPassenger.vue
@@ -1,26 +1,38 @@
 <template>
   <v-row>
     <v-col>
-      <v-row
-        v-for="(leg, index) in generateShoutOutSteps()"
-        :key="index"
-        class="mx-3"
-      >
+      <v-row v-for="(leg, index) in legs" :key="index" class="mx-3">
         <itinerary-leg
           :leg="leg"
           :showicon="showicon"
           :showdottedline="showdottedline"
         />
       </v-row>
+      <v-row v-if="selectedOffer != null">
+        <v-col>
+          <v-btn
+            block
+            rounded
+            depressed
+            outlined
+            color="primary"
+            @click="onClear"
+          >
+            Opnieuw
+          </v-btn>
+        </v-col>
+        <v-col>
+          <v-btn block rounded depressed color="button">
+            Bevestigen
+          </v-btn>
+        </v-col>
+      </v-row>
       <v-row dense class="d-flex flex-column">
         <v-col><v-divider /></v-col>
-        <v-col>
+        <v-col class="mt-2">
           <h3>Rit aanbod</h3>
         </v-col>
-        <v-col
-          v-if="!trip.itineraries || trip.itineraries.length == 0"
-          class="py-3"
-        >
+        <v-col v-if="!trip.itineraries || trip.itineraries.length == 0">
           <em>Er zijn nog geen ritten aangeboden.</em>
         </v-col>
         <v-col v-else class="py-3">
@@ -42,7 +54,10 @@
 </template>
 
 <script>
-import { generateShoutOutDetailSteps } from '@/utils/itinerary_steps.js'
+import {
+  generateShoutOutDetailSteps,
+  generateItineraryDetailSteps,
+} from '@/utils/itinerary_steps.js'
 import ItineraryLeg from '@/components/itinerary-details/ItineraryLeg.vue'
 import TravelProposalSummary from '@/components/community/TravelProposalSummary.vue'
 
@@ -67,13 +82,20 @@ export default {
     showdottedline() {
       return this.selectedOffer == null
     },
+    legs() {
+      if (this.selectedOffer == null) {
+        return generateShoutOutDetailSteps(this.trip)
+      }
+      return generateItineraryDetailSteps(this.selectedOffer)
+    },
   },
   methods: {
-    generateShoutOutSteps() {
-      return generateShoutOutDetailSteps(this.trip)
-    },
     onTravelProposalSelected(itinerary) {
       console.log('onTravelOfferSelected: ', itinerary)
+      this.selectedOffer = itinerary
+    },
+    onClear() {
+      this.selectedOffer = null
     },
   },
 }
