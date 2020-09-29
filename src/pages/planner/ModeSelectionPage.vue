aec6d4f6e616194fc478a9abc57e3c6027ddfcf4
J-D-Heijink <j.d.heijink@saxion.nl>
2020-09-29 14:34:28 +0200

    v-flex en v-layout schoonmaak

--- src/pages/planner/ModeSelectionPage.vue
+++ src/pages/planner/ModeSelectionPage.vue
@@ -7,68 +7,33 @@
     grid-list-lg
     class="background-primary"
   >
-    <v-layout justify-center align-center>
-      <v-flex xs11 sm9 md6>
-        <v-layout column shrink>
-          <v-flex class="box-widget background-white">
-            <v-flex>
-              <v-form>
-                <v-layout>
-                  <v-flex xs12>
-                    <h1>Wat wil je doen?</h1>
-                  </v-flex>
-                </v-layout>
-
-                <v-layout mt-2 column justify-center text-xs-center>
-                  <v-flex>
-                    <v-layout column>
-                      <v-flex>
-                        <v-btn
-                          large
-                          rounded
-                          depressed
-                          color="button"
-                          block
-                          to="/search"
-                        >
-                          Een reis plannen
-                        </v-btn>
-                      </v-flex>
-                      <v-flex py-0>
-                        <span class="mid-grey">
-                          Met iemand meerijden of via het OV.
-                        </span>
-                      </v-flex>
-                    </v-layout>
-                  </v-flex>
-                  <v-flex mt-4>
-                    <v-layout column>
-                      <v-flex>
-                        <v-btn
-                          large
-                          rounded
-                          depressed
-                          color="button"
-                          block
-                          to="/plan"
-                        >
-                          Een rit aanbieden
-                        </v-btn>
-                      </v-flex>
-                      <v-flex py-0>
-                        <span class="mid-grey">
-                          Zelf rijden en iemand meenemen.
-                        </span>
-                      </v-flex>
-                    </v-layout>
-                  </v-flex>
-                </v-layout>
-              </v-form>
-            </v-flex>
-          </v-flex>
-        </v-layout>
-      </v-flex>
-    </v-layout>
+    <v-row class="justify-center">
+      <v-col cols="11" class="box-widget background-white shrink flex sm9 xs11">
+        <v-row>
+          <v-col><h1>Wat wil je doen?</h1></v-col>
+        </v-row>
+        <v-row class="mt-2 px-2">
+          <v-btn large rounded depressed color="button" block to="/search">
+            Een reis plannen
+          </v-btn>
+        </v-row>
+        <v-row class="pa-2">
+          <span class="mid-grey">
+            Met iemand meerijden of via het OV.
+          </span>
+        </v-row>
+        <v-row class="mt-5 px-2">
+          <v-btn large rounded depressed color="button" block to="/plan">
+            Een rit aanbieden
+          </v-btn>
+        </v-row>
+        <v-row class="pa-2">
+          <span class="mid-grey">
+            Zelf rijden en iemand meenemen.
+          </span>
+        </v-row>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
