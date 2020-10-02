140876d60c3012ddf283404887a5b64946260db6
Timothy Sealy <timothy.sealy@gmail.com>
2020-10-02 08:41:17 +0200

    Updated charity detail page to new charity structure and store actions.

--- src/pages/community/goals/CharityDetailsPage.vue
+++ src/pages/community/goals/CharityDetailsPage.vue
@@ -4,7 +4,6 @@
       <v-col>
         <v-img
           src="@/assets/achterhoek_background.jpg"
-          lazy-src="https://picsum.photos/id/11/100/60"
           aspect-ratio="1"
           class="goal-jumbo-image"
           max-width="500"
@@ -23,8 +22,8 @@
     </v-row>
     <v-row>
       <v-col class="pt-0">
-        <h1 class="netmobiel">{{ charity ? charity.name : '' }}</h1>
-        <span class="overline">{{ charity ? charity.place : '' }}</span>
+        <h1 class="netmobiel">{{ charityName }}</h1>
+        <span class="overline">{{ charityLocation }}</span>
       </v-col>
     </v-row>
     <v-row>
@@ -36,22 +35,18 @@
     </v-row>
     <v-row>
       <v-col>
-        <goal-progress-bar
-          name="Donateurs"
-          :value-current="charity.totalDonors"
-        >
-        </goal-progress-bar>
+        <goal-progress-bar name="Donateurs" :value-current="totalDonors" />
       </v-col>
       <v-col>
-        <goal-progress-bar name="Credits" :value-current="charity.credits">
+        <goal-progress-bar name="Credits" :value-current="donatedAmount">
         </goal-progress-bar>
       </v-col>
       <v-col>
         <goal-progress-bar
           name="Behaald"
           :display-percentage="true"
-          :value-current="charity.credits"
-          :value-total="charity.goal"
+          :value-current="donatedAmount"
+          :value-total="goalAmount"
         >
         </goal-progress-bar>
       </v-col>
@@ -103,10 +98,29 @@ export default {
     donations() {
       return chsStore.getters.getSelectedCharityDonations
     },
+    charityImage() {
+      return '@/assets/achterhoek_background.jpg'
+    },
+    charityName() {
+      console.log(this.charity)
+      return this.charity?.name || ''
+    },
+    charityLocation() {
+      return this.charity?.location?.label || ''
+    },
+    totalDonors() {
+      return this.donations?.length || 0
+    },
+    donatedAmount() {
+      return this.charity?.donatedAmount || 0
+    },
+    goalAmount() {
+      return this.charity?.goalAmount || 1
+    },
   },
   created() {
     uiStore.mutations.showBackButton()
-    chsStore.actions.lookupCharity(this.id)
+    chsStore.actions.fetchCharity(this.id)
     chsStore.actions.fetchDonationsFromCharity(this.id)
   },
   methods: {
