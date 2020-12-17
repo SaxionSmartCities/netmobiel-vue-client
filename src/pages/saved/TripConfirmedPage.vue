16977a56fbb63c7b93078fea010164be14305ffe
Timothy Sealy <timothy.sealy@gmail.com>
2020-12-17 12:13:05 +0100

    Moved trip confirmed page to saved.

---
+++ src/pages/saved/TripConfirmedPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <content-pane>
+    <v-row>
+      <v-col>
+        <h1>Rit bevestigd! 🎉</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        Bedankt voor jouw bevestiging!
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn block rounded outlined color="primary" to="/tripsOverviewPage">
+          Terug naar ritten
+        </v-btn>
+      </v-col>
+    </v-row>
+  </content-pane>
+</template>
+
+<script>
+import ContentPane from '@/components/common/ContentPane.vue'
+
+export default {
+  name: 'TripConfirmedPage',
+  components: { ContentPane },
+}
+</script>
+
+<style lang="scss"></style>
