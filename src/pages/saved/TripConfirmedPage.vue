a5386b0d3f4393f4bd2be7eb3c99d6454fc2af63
Timothy Sealy <timothy.sealy@gmail.com>
2020-12-17 16:29:07 +0100

    Updated trip confirm page to include the review of the driver.

--- src/pages/saved/TripConfirmedPage.vue
+++ src/pages/saved/TripConfirmedPage.vue
@@ -12,7 +12,64 @@
     </v-row>
     <v-row>
       <v-col>
-        <v-btn block rounded outlined color="primary" to="/tripsOverviewPage">
+        <span class="body-2">
+          Hoe heb jij deze rit ervaren? Geef jouw mening en laat
+          <strong>{{ driverName }}</strong> weten wat jij er van vond.
+        </span>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <div class="mt-4">
+          <div v-if="showChips && availableCompliments">
+            <v-chip
+              v-for="compliment in availableCompliments"
+              :key="compliment"
+              :ripple="false"
+              class="compliment-chip body-2"
+              :class="{
+                'compliment-chip-active':
+                  compliments.findIndex(c => c === compliment) !== -1,
+              }"
+              :value="compliment"
+              @click="addCompliment(compliment)"
+            >
+              {{ compliment }}
+            </v-chip>
+          </div>
+          <div>
+            <v-textarea
+              v-model="inputTextArea"
+              class="pt-2"
+              placeholder="Beschrijving..."
+              outlined
+            ></v-textarea>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn
+          block
+          large
+          depressed
+          rounded
+          color="button"
+          :disabled="compliments.length === 0"
+        >
+          Beoordeling opslaan
+        </v-btn>
+      </v-col>
+      <v-col>
+        <v-btn
+          block
+          large
+          rounded
+          outlined
+          color="primary"
+          to="/tripsOverviewPage"
+        >
           Terug naar ritten
         </v-btn>
       </v-col>
@@ -22,11 +79,74 @@
 
 <script>
 import ContentPane from '@/components/common/ContentPane.vue'
+import constants from '@/constants/constants'
+import * as psStore from '@/store/profile-service'
+import * as isStore from '@/store/itinerary-service'
 
 export default {
   name: 'TripConfirmedPage',
   components: { ContentPane },
+  data() {
+    return {
+      compliments: [],
+      feedbackMessage: '',
+      showChips: true,
+      inputTextArea: null,
+    }
+  },
+  computed: {
+    trip() {
+      return isStore.getters.getSelectedTrip
+    },
+    driverName() {
+      const found = this.trip.itinerary?.legs.find(
+        l => l.driverName !== undefined
+      )
+      return found?.driverName || 'Onbekende chauffeur'
+    },
+    availableCompliments() {
+      return psStore.getters.getComplimentTypes
+    },
+  },
+  mounted() {
+    psStore.actions.fetchComplimentTypes()
+  },
+  methods: {
+    addCompliment(compliment) {
+      const index = this.compliments.findIndex(c => c === compliment)
+      if (
+        this.compliments.length < constants.maxComplimentsAllowed &&
+        index === -1
+      ) {
+        //Not reached the limit of compliments and the compliment is NOT in the list yet.
+        this.compliments.push(compliment)
+      } else if (
+        // Limit reached and the compliment is not in the list.
+        // Remove the first compliment selected and add the one selected
+        this.compliments.length === constants.maxComplimentsAllowed &&
+        index === -1
+      ) {
+        this.compliments.splice(index, 1)
+        this.compliments.push(compliment)
+      } else {
+        // If a compliment gets selected when it already is, then it will be removed.
+        this.compliments.splice(index, 1)
+      }
+    },
+  },
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.compliment-chip {
+  margin: 3px;
+  box-shadow: none;
+  border: none;
+  color: $color-primary;
+  background: rgba($color-primary, 0.05) !important;
+}
+.compliment-chip-active {
+  color: white;
+  background: $color-primary !important;
+}
+</style>
