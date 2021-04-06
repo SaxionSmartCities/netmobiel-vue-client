16bc2c276cfd6a7a7589712a8803254108c0fefa
Timothy Sealy <timothy.sealy@gmail.com>
2021-04-06 14:41:06 +0200

    Added page (including routing) for adding a new delegation.

---
+++ src/pages/profile/delegation/NewDelegationPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <content-pane>
+    <v-row>
+      <v-col>
+        New Account
+      </v-col>
+    </v-row>
+  </content-pane>
+</template>
+
+<script>
+import ContentPane from '@/components/common/ContentPane'
+
+export default {
+  name: 'DelegationOverview',
+  components: { ContentPane },
+}
+</script>
