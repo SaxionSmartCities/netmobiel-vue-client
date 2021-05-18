511384fd2b208721983b4e7ebeb4de6598fade60
Timothy Sealy <timothy.sealy@gmail.com>
2021-05-18 15:28:43 +0200

    Extended add delegation page to search and show results and route to add new delegation page.

--- src/pages/profile/delegation/AddDelegationPage.vue
+++ src/pages/profile/delegation/AddDelegationPage.vue
@@ -2,7 +2,51 @@
   <content-pane>
     <v-row>
       <v-col>
-        <h4>Account toevoegen</h4>
+        <h3>Account toevoegen</h3>
+        <span>
+          Zoek naar een bestaand account om te beheren of maak een nieuwe aan.
+        </span>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <h4 class="text-primary">Zoek bestaande account</h4>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="pt-0">
+        <v-text-field
+          v-model="search"
+          clearable
+          hide-details
+          outlined
+          required
+          label="Telefoon / Email"
+          @click:clear="clearSearchInput"
+        ></v-text-field>
+      </v-col>
+    </v-row>
+    <v-row v-if="showResults">
+      <v-col>
+        <h4>Resultaten</h4>
+        <account-list
+          :accounts="searchResults"
+          empty-list-label="Geen accounts gevonden"
+        />
+      </v-col>
+    </v-row>
+    <v-row v-else>
+      <v-col>
+        <v-divider class="pb-4" />
+        <v-btn
+          rounded
+          block
+          outlined
+          color="primary"
+          to="/profile/delegate/new"
+        >
+          Nieuw account toevoegen
+        </v-btn>
       </v-col>
     </v-row>
   </content-pane>
@@ -10,11 +54,55 @@
 
 <script>
 import ContentPane from '@/components/common/ContentPane'
+import AccountList from '@/components/lists/AccountList'
+import { throttle } from 'lodash'
+import * as psStore from '@/store/profile-service'
+import * as uiStore from '@/store/ui'
 
 export default {
   name: 'AddDelegationPage',
-  components: { ContentPane },
+  components: { ContentPane, AccountList },
+  data() {
+    return {
+      search: '',
+      showResults: false,
+    }
+  },
+  computed: {
+    searchResults() {
+      return psStore.getters.getSearchResults
+    },
+  },
+  watch: {
+    search: throttle(function(val) {
+      if (val != null && val.length > 3) {
+        this.showResults = true
+        psStore.actions.fetchProfiles({ keyword: val })
+      } else {
+        this.showResults = false
+        psStore.mutations.setSearchResults([])
+      }
+    }, 500),
+  },
+  mounted() {
+    uiStore.mutations.showBackButton()
+  },
+  methods: {
+    clearSearchInput() {
+      this.search = ''
+    },
+  },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.text-primary {
+  color: $color-primary !important;
+}
+
+.primary-border {
+  background-color: #2e899720;
+  border: 1px solid $color-primary;
+  border-radius: 10px;
+}
+</style>
