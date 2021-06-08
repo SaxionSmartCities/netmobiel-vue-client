fac730461ef6d36c0dc1dfb62ce1e133217f4198
Timothy Sealy <timothy.sealy@gmail.com>
2021-06-08 17:02:04 +0200

    Basic image upload page (still needs work).

---
+++ src/components/onboarding/NewDelegatorImage.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row no-gutters>
+    <v-col dense>
+      <v-row>
+        <v-col class="pb-0" align="center">
+          Foto
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <h4 class="netmobiel">
+            Wat moet de chauffeur verder weten?
+          </h4>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          Kan <em>{{ value.firstName }}</em> bij aankomst vervoerder direct
+          instappen?
+        </v-col>
+      </v-row>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: 'NewDelegationImage',
+  props: {
+    value: {
+      type: Object,
+      default: () => undefined,
+    },
+  },
+}
+</script>
