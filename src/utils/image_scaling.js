76300881813cbb029de5b219825143de93a40bac
Timothy Sealy <timothy.sealy@gmail.com>
2021-06-29 18:02:16 +0200

    Revert "PATCH: Disabled foto downscaling for testing purposes."
    This reverts commit b7c0af6b5aee8ecddecfb3c0201ea8b0529a8efb.

--- src/utils/image_scaling.js
+++ src/utils/image_scaling.js
@@ -43,8 +43,7 @@ export function scaleImageDown(image, target) {
   const originalFileSize = getFileSize(image)
   return new Promise(resolve => {
     getImageDimensions(image).then(({ height, width }) => {
-      // const resizeFactor = Math.ceil(originalFileSize / target)
-      const resizeFactor = 1
+      const resizeFactor = Math.ceil(originalFileSize / target)
       resolve(resizeImage(image, width / resizeFactor, height / resizeFactor))
     })
   })
