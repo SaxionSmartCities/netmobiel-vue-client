1111637bff49b2a7cb0b5787880aefde126fc7f3
Jaap Reitsma <jaap.reitsma@vidinexus.com>
2021-07-02 19:55:28 +0200

    Created logout page.

---
+++ src/pages/home/LogoutPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <content-pane>
+    <v-row align="center">
+      <v-col>
+        <h1>Tot ziens</h1>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="px-1 pt-0">
+        <span>
+          Tot de volgende keer bij Netmobiel, dé mobiliteitsapp van de
+          Achterhoek en omstreken.
+        </span>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="px-1 pt-0">
+        <span>
+          U wordt nu afgemeld...
+        </span>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="px-1 pt-0">
+        <v-progress-circular indeterminate class="rotate"></v-progress-circular>
+      </v-col>
+    </v-row>
+  </content-pane>
+</template>
+
+<script>
+import ContentPane from '@/components/common/ContentPane.vue'
+
+export default {
+  components: {
+    ContentPane,
+  },
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss"></style>
