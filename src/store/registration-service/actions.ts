e5d3b0a1c8884ac2c463a9fe49500dbc09a5b56b
Timothy Sealy <timothy.sealy@gmail.com>
2021-07-13 14:49:44 +0200

    Wrapped profile creation request in a promise.

--- src/store/registration-service/actions.ts
+++ src/store/registration-service/actions.ts
@@ -5,6 +5,7 @@ import { BareActionContext, ModuleBuilder } from 'vuex-typex'
 import { RegistrationRequest, RegistrationState } from './types'
 import { mutations } from '@/store/registration-service'
 import { RootState } from '@/store/Rootstate'
+import * as uiStore from '@/store/ui'
 type ActionContext = BareActionContext<RegistrationState, RootState>
 
 const { PROFILE_BASE_URL, GRAVITEE_PROFILE_SERVICE_API_KEY } = config
@@ -13,33 +14,38 @@ const { generateHeaders } = util
 function submitRegistrationRequest(payload: RegistrationRequest) {
   const URL = `${PROFILE_BASE_URL}/profiles`
   mutations.storeRegistrationRequest(payload)
-  return axios
-    .post(URL, payload, {
-      headers: generateHeaders(GRAVITEE_PROFILE_SERVICE_API_KEY),
-    })
-    .then(function() {
-      mutations.setRegistrationStatus({ success: true, message: '' })
-    })
-    .catch(function(error) {
-      const status = error.response.status
-      let errorMsg = ''
-      if (status === 422) {
-        errorMsg = 'Ontbrekende data (email, voornaam of achternaam).'
-      } else if (status === 451) {
-        errorMsg =
-          'Ga akkoord  met de voorwaarden en bevestig 16 jaar of ouder te zijn.'
-      } else if (status === 500) {
-        // No clue what is going on, but the server should report something about it
-        errorMsg = error.response.data.message || error.response.data
-      } else if (status === 409) {
-        errorMsg = 'Het emailadres is al in gebruik.'
-      }
-
-      mutations.setRegistrationStatus({
-        success: false,
-        message: errorMsg,
+  return new Promise((resolve, reject) => {
+    axios
+      .post(URL, payload, {
+        headers: generateHeaders(GRAVITEE_PROFILE_SERVICE_API_KEY),
       })
-    })
+      .then(response => {
+        console.log(response)
+        mutations.setRegistrationStatus({ success: true, message: '' })
+        resolve(response)
+      })
+      .catch(error => {
+        const status = error.response.status
+        let errorMsg = ''
+        if (status === 422) {
+          errorMsg = 'Ontbrekende data (email, voornaam of achternaam).'
+        } else if (status === 451) {
+          errorMsg =
+            'Ga akkoord  met de voorwaarden en bevestig 16 jaar of ouder te zijn.'
+        } else if (status === 500) {
+          // No clue what is going on, but the server should report something about it
+          errorMsg = error.response.data.message || error.response.data
+        } else if (status === 409) {
+          errorMsg = 'Het emailadres is al in gebruik.'
+        }
+
+        mutations.setRegistrationStatus({
+          success: false,
+          message: errorMsg,
+        })
+        reject(error)
+      })
+  })
 }
 
 function newAccountRegistration(
@@ -53,15 +59,19 @@ function newDelegateRegistration(
   context: ActionContext,
   payload: RegistrationRequest
 ) {
+  // 1. Create profile
   mutations.clearRegistrationRequest()
   submitRegistrationRequest(payload)
-    .then(() => {
-      //TODO: Set delegation.
+    .then(response => {
+      // 2. Create delegation
       console.log('Success!')
     })
     .catch(error => {
       // eslint-disable-next-line
       console.log(error)
+      uiStore.actions.queueErrorNotification(
+        `Fout bij het aanmaken van het profiel`
+      )
     })
 }
 
