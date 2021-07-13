de94bfdc11eba4944fe09a3e8e0f71bb0ced3b75
Timothy Sealy <timothy.sealy@gmail.com>
2021-07-13 11:58:19 +0200

    Moved onboarding component to page.

--- src/pages/profile/delegation/NewDelegationPage.vue
+++ src/pages/profile/delegation/NewDelegationPage.vue
@@ -1,19 +1,120 @@
 <template>
   <content-pane>
-    <onboarding-navigation title="Nieuw account" />
+    <v-row>
+      <v-col class="pt-0">
+        <v-row align="end">
+          <v-col>
+            <h3>{{ titles[currStep - 1] }}</h3>
+          </v-col>
+          <v-col class="shrink stappen caption text--gray"
+            >Stap {{ currStep }} / {{ maxSteps }}
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col v-if="currStep === 1" class="pt-0">
+            <new-delegation
+              v-model="registrationRequest"
+              @onFormValid="currentFormValid"
+            />
+          </v-col>
+          <v-col v-if="currStep === 2">
+            <delegation-options v-model="registrationRequest" />
+          </v-col>
+          <v-col v-if="currStep === 3">
+            <delegation-comms-settings v-model="registrationRequest" />
+          </v-col>
+          <v-col v-if="currStep === 4">
+            <new-delegation-image v-model="registrationRequest" />
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col xs6>
+            <v-btn
+              block
+              rounded
+              outlined
+              color="primary"
+              :disabled="currStep === 1"
+              @click="prevStep()"
+            >
+              Terug
+            </v-btn>
+          </v-col>
+          <v-col v-if="currStep !== maxSteps" xs6>
+            <v-btn
+              block
+              rounded
+              depressed
+              color="button"
+              :disabled="!nextStepEnabled"
+              @click="nextStep()"
+            >
+              Verder
+            </v-btn>
+          </v-col>
+          <v-col v-else xs6>
+            <v-btn block rounded depressed color="button" @click="complete()">
+              Aanmaken
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
   </content-pane>
 </template>
 
 <script>
 import ContentPane from '@/components/common/ContentPane'
-import OnboardingNavigation from '@/components/onboarding/OnboardingNavigation.vue'
+import NewDelegation from '@/components/onboarding/NewDelegation'
+import DelegationOptions from '@/components/onboarding/NewDelegatorOptions'
+import DelegationCommsSettings from '@/components/onboarding/DelegationCommsSettings'
+import NewDelegationImage from '@/components/onboarding/NewDelegatorImage'
 import * as uiStore from '@/store/ui'
 
 export default {
   name: 'DelegationOverview',
-  components: { ContentPane, OnboardingNavigation },
+  components: {
+    ContentPane,
+    NewDelegation,
+    DelegationOptions,
+    DelegationCommsSettings,
+    NewDelegationImage,
+  },
+  data() {
+    return {
+      currStep: 1,
+      maxSteps: 4,
+      nextStepEnabled: false,
+      titles: [
+        'Nieuw account',
+        'Ritvoorkeuren',
+        'Communicatie',
+        'Profiel foto',
+      ],
+      registrationRequest: {
+        extraTransferTime: false,
+        consent: {
+          olderThanSixteen: true,
+        },
+      },
+    }
+  },
   mounted() {
     uiStore.mutations.showBackButton()
+  },
+  methods: {
+    nextStep() {
+      this.currStep++
+    },
+    prevStep() {
+      this.currStep--
+    },
+    complete() {
+      console.log(`TODO: onComplete ${this.registrationRequest}`)
+    },
+    currentFormValid(isValid) {
+      this.nextStepEnabled = isValid
+    },
   },
 }
 </script>
